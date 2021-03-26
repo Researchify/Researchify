803a13117439d4a6135d889d2fae0521091ec153
akshay <akshay.k1738@gmail.com>
2021-03-25 20:52:53 -0500

    set up dockerfiles and update readme instructions

--- client/src/api/fooApi.js
+++ client/src/api/fooApi.js
@@ -1,10 +1,15 @@
 /**
- * This file exports a custom axios client for our own API server which handles all requests associated with posts.
+ * This file exports a custom axios client for our own API server which handles all requests associated with foos.
+ *
+ * @note: If this React client is being served by a reverse proxy, the localServerUrl is used as a *relative*
+ * URL to point to the "/api" route, and the reverse proxy is expected to proxy the request to the API server.
+ * see: https://developer.mozilla.org/en-US/docs/Learn/Common_questions/What_is_a_URL#absolute_urls_vs_relative_urls
  */
 import axios from 'axios';
 
 
-const localServerUrl = 'http://localhost:5000';
+const usingRp = true;
+const localServerUrl = usingRp ? '/api' : 'http://localhost:5000';
 
 export default axios.create({
     baseURL: localServerUrl
