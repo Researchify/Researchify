3b6e2ad6fceabe0bd4336c5b9b7e6cfc81d86d9e
akshay <akshay.k1738@gmail.com>
2021-04-08 19:42:15 -0500

    Comment-out unused react imports in test

--- client/src/test/App.test.js
+++ client/src/test/App.test.js
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "../components/App";
+// import React from "react";
+// import ReactDOM from "react-dom";
+//
+// import App from "../components/App";
 
 
 // test('renders w/out crashing', () => {
