8c7e7bc81e7fd4f6ae37604f1b06209bff89ca86
Sharan Sharabinth <ssha0054@student.monash.edu>
2021-04-09 17:21:26 +1000

    Refactor login and register backend

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,40 +1,14 @@
 /**
- * This module defines endpoints for the "/foo" route and exports its corresponding Router
+ * This module defines endpoints for the "/users" route and exports its corresponding Router
  */
-const router = require('express').Router();
+const usersRouter = require('express').Router();
 
-let User = require('../models/user.model');
+const usersController = require('../controllers/users');
 
-router.get('/', (req, res) => {
-  User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
+usersRouter.get('/', usersController.getUsers);
 
-router.post('/', (req, res) => {
-    const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
-  
-    newUser.save()
-      .then(() => res.json('User added!'))
-      .catch(err => res.status(400).json('Error: ' + err));
-});
+usersRouter.post('/', usersController.addUser);
 
-router.post('/login', (req, res) => {
-    User.findOne({email: req.body.email})
-        .then(user => {
-            if (user == null) {
-                res.status(400).send("User not found");
-            } else if (user.password != req.body.password) {
-                res.status(400).send("Incorrect password");
-            } else {
-                res.send("Successfully logged in");
-            }
-        })
-        .catch(err => res.status(400).json('Error: ' + err));
-});
+usersRouter.post('/login', usersController.loginUser);
 
-module.exports = router;
+module.exports = usersRouter;
