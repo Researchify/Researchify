29c6233576cb896136688a5feebe13ab44c87e62
Kunj Dave <kdav0006@student.monash.edu>
2021-04-10 19:05:52 +1000

    Convert class components to functional components

--- client/src/components/auth/PasswordWithConfirmation.js
+++ client/src/components/auth/PasswordWithConfirmation.js
@@ -1,39 +1,33 @@
 /**
- * This file exports an Password component used to display password input form with confirmation.
+ * This file exports a Password component used to display password input form with confirmation.
  */
 
- import React from 'react';
+ import React, { useState } from 'react';
  import Form from 'react-bootstrap/Form'
  
-class PasswordWithConfirmation extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            password: '',
-            confirmPassword: ''
-        }
-    }
-    
-    updateValue = form => {
-        const {name, value} = form.target
-        this.setState({[name]: value})
-    }
+ export default function PasswordWithConfirmation() {
+     const [passwordDetails, setpwDetails] = useState({
+        password: '',
+        confirmPassword: ''
+    });
 
-    render() {
-        console.log("password:" + this.state.password + "confirm password:" +this.state.confirmPassword);
-        return(
-            <Form>
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" onChange={this.updateValue} name="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label>Confirm Password</Form.Label>
-                <Form.Control type="password" onChange={this.updateValue} name="confirmPassword" placeholder="Password" />
-            </Form.Group>
-            </Form>
-        );
+    const updateDetails = form => {
+        const {name, value} = form.target;
+        setpwDetails({[name]: value});
     }
-}
+    console.log("password:" + passwordDetails.password + "\n confirm password:" +passwordDetails.confirmPassword);
 
- export default PasswordWithConfirmation;
+    return(
+        <Form>
+        <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" onChange={updateDetails} name="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control type="password" onChange={updateDetails} name="confirmPassword" placeholder="Password" required/>
+        </Form.Group>
+        </Form>
+    );
+
+ }
--- client/src/components/auth/RegistrationForm.js
+++ client/src/components/auth/RegistrationForm.js
@@ -2,46 +2,33 @@
  * This file exports a Registration Form component used to display registration input.
  */
 
- import React from 'react';
- import Form from 'react-bootstrap/Form';
+ import React, { useState } from 'react';
  import Jumbotron from 'react-bootstrap/Jumbotron';
  import Button from 'react-bootstrap/Button';
  import Email from './Email';
  import PasswordWithConfirmation from './PasswordWithConfirmation';
  import FullName from './FullName';
- import {Row} from "react-bootstrap";
  import '../register/Register.css';
 
- class RegistrationForm extends React.Component {
-     constructor(props) {
-         super(props)
-         this.state = {
-             email: '',
-             name: '',
-             email: '',
-             password: ''
-         }
-     }
-     updateUserData(data, dataType) {
-        switch (dataType) {
-            case 'email':
-                this.setState(...this.state, {email: data});
-        }
-     }
-    render() {
-        return(
-            <Jumbotron id='form-box'>
-                <h3 id='signUpHeading'>Sign Up</h3>
-                <hr/>
-                <FullName/>
-                <Email updateData={this.updateUserData}/>
-                <PasswordWithConfirmation/>
-                <div>
-                    <a id='signInLink' href="url">Already have an account? Sign in</a>
-                    <Button id = 'submitButton' type="submit" variant="primary">Sign Up</Button>
-                </div>
-            </Jumbotron>
-        );
-    }
-}
- export default RegistrationForm;
+ export default function RegistrationForm() {
+     const [inputs, setInputs] = useState({
+        email: '',
+        name: '',
+        email: '',
+        password: ''
+    });
+
+    return(
+        <Jumbotron id='form-box'>
+            <h3 id='signUpHeading'>Sign Up</h3>
+            <hr/>
+            <FullName/>
+            <Email />
+            <PasswordWithConfirmation/>
+            <div>
+                <a id='signInLink' href="login">Already have an account? Sign in</a>
+                <Button id = 'submitButton' type="submit" variant="primary">Sign Up</Button>
+            </div>
+        </Jumbotron>
+    );
+ }
