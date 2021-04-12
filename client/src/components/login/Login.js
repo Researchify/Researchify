3b2c3bb5a25c1cc505e2e003c7d641d64b7c82c1
rsal0007 <rsal0007@student.monash.edu>
2021-04-13 02:38:34 +0530

    Added UI for user login page.

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,9 +1,14 @@
 /**
  * This file exports an Auth component used to display sign-ins and sign-ups.
  */
-import React from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import React, { useState } from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form'
+import './Login.css';
+import NavigationBar from '../auth/NavigationBar';
 
+/** 
 const Login = () => {
     return (
         <div>
@@ -20,3 +25,65 @@ const Login = () => {
 };
 
 export default Login;
+
+
+
+*/
+ export default function Login() {
+    const [inputs, setInputs] = useState({
+        email: '',
+        password: '',
+    });
+
+    const updateValue = form => {
+        const {name, value} = form.target
+        setInputs({...inputs, [name]: value})
+    };
+
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+      const form = event.currentTarget;
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+  
+      setValidated(true);
+    };
+
+    return(
+        <div>
+            <NavigationBar/>
+            <div id='login-page'>
+        <Jumbotron id='form-box'>
+        
+        <h3 id='LoginHeading'>Log In</h3>
+        <hr/>
+        
+        <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" onChange={updateValue} name="email" placeholder="Enter email" required/>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" onChange={updateValue} name="password" placeholder="Password" required/>
+                </Form.Group>
+                
+                <Button id = 'loginButton' type="submit" variant="primary">Log in</Button>
+                <div>
+                <a id='loginLink' href="login">Don't have an account yet? Sign Up today! </a>
+                
+            </div>
+
+            
+            
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            
+            </Form>
+        </Jumbotron>
+        </div>
+    </div>
+    );
+ }
