3bd0994b389043bbc08ffcb4c662f69294d5d3ad
rpra0006 <rpra0006@student.monash.edu>
2021-04-13 12:46:16 +0700

    Adding profile info edit page
    This will link to dashboard where user can edit their profile information

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,17 +6,23 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Auth from './auth/Auth';
 import Home from './home/Home';
+import ProfileInfoEdit from './profileInfoEdit/ProfileInfoEdit';
 
 
 const App = () => {
     return (
+        
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Home}/>
                 <Route path="/auth" exact component={Auth}/>
+                <Route path="/editprofile" exact component={ProfileInfoEdit} />
             </Switch>
         </BrowserRouter>
     );
 };
 
+/**
+ * <Route path="/editprofile" exact component={ProfileInfoEdit} />
+ */
 export default App;
