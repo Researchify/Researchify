8cc8d1a913b7592eb274f8455d0c9cf7952ca888
Sanath Mahesh <sana0007@student.monash.edu>
2021-04-15 02:47:16 +0400

    initial home page

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -2,25 +2,13 @@
  * This file exports a home component that displays the main screen/homepage.
  */
 import React from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-
-
-import {getFoos} from '../../actions/foo';
-
+import './home.css'
 
 const Home = () => {
-    const dispatch = useDispatch();
-    const foos = useSelector(state => state.foo);
-
     return (
-        <div>
-            Home Component.
-            <br />
-            <button onClick={() => dispatch(getFoos())}>Click To Get Foos</button>
-            <br />
-            {
-                foos
-            }
+        <div className="homePage">
+            <div className="homePageTitleBox">
+            </div>
         </div>
     );
 };
