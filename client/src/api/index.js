653fd75859dc02a9e1fe61bb730803822aaafb08
kdav <63642262+kdav108@users.noreply.github.com>
2021-04-15 11:53:38 +1000

    Revert "Updates branch thunder"

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,13 +3,7 @@
  */
 import fooApi from './fooApi';
 import publicationsApi from './publications';
-import userApi from "./userApi";
 
-/**
- * Posts the user data (email, given and family name and password) to api at endpoint '/'
- * @param {*} payload object containing email, givenName, familyName and password of the user
- */
-export const addUserData = (payload) => userApi.post("/users", payload);
 
 export const fetchFoos = () => fooApi.get('/foo');
 
