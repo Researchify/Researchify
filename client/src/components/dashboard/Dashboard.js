11e12bffce6779460466ba4745d545181dfbb251
Jie Fan <jfan0010@student.monash.edu>
2021-04-20 21:24:27 +1000

    trial with react-bootstrap

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Layout from '../layout/Layout'
 import './Dashboard.css'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import cardPicture from './banana-leaf-macro-green-micro-banner.jpg'
 
 function PageContent() {
     return (
@@ -9,13 +12,29 @@ function PageContent() {
 
                 <div class="mdl-grid">
 
+                    <Card className="page-card">
+                        <Card.Img className="page-img" variant="top" src={cardPicture} />
+                            <Card.ImgOverlay>
+                                <Card.Link className="page-share-icon" href='#'>Share Page Icon</Card.Link>
+                                <Card.Text className="page-title">Page 1</Card.Text>
+                            </Card.ImgOverlay>
+                        <Card.Body>
+                            <Card.Text>
+                                Description of Page 1..... (btw this card is using react-bootstrap)
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Body>
+                            <Card.Link className="page-manage" href='#'>Manage Page</Card.Link>
+                        </Card.Body>
+                    </Card>
+
                     <div class="mdl-cell mdl-cell--8-col">
                         <div class="mdl-card mdl-shadow--3dp">
                             <div class="mdl-card__title">
-                                <h2 class="mdl-card__title-text">Page 1</h2>
+                                <h2 class="mdl-card__title-text">Page 2</h2>
                             </div>
                             <div class="mdl-card__supporting-text">
-                                Description of Page 1...... 
+                                Description of Page 2......
                             </div>
                             <div class="mdl-card__actions mdl-card--border">
                                 <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
@@ -29,35 +48,14 @@ function PageContent() {
                             </div>
                         </div>
                     </div>
-                    
+
                     <div class="mdl-cell mdl-cell--8-col">
                         <div class="mdl-card mdl-shadow--3dp">
                             <div class="mdl-card__title">
-                                <h2 class="mdl-card__title-text">Page 2</h2>
+                                <h2 class="mdl-card__title-text">Page 3</h2>
                             </div>
                             <div class="mdl-card__supporting-text">
-                                Description of Page 2...... 
-                            </div>
-                            <div class="mdl-card__actions mdl-card--border">
-                                <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-                                    Manage Page
-                                </a>
-                            </div>
-                            <div class="mdl-card__menu">
-                                <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
-                                    <i class="material-icons">share</i>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <div class="mdl-cell mdl-cell--8-col">
-                        <div class="mdl-card mdl-shadow--3dp">
-                            <div class="mdl-card__title">
-                                <h2 class="mdl-card__title-text">Page 2</h2>
-                            </div>
-                            <div class="mdl-card__supporting-text">
-                                Description of Page 2...... 
+                                Description of Page 3......
                             </div>
                             <div class="mdl-card__actions mdl-card--border">
                                 <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
