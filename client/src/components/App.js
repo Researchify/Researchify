a02e5d0edcc42b101eec4cecb9324ea1752feb95
jfan0010 <48777884+jfan0010@users.noreply.github.com>
2021-04-22 17:06:58 +1000

    Added lines deleted in client/src/components/App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,6 +6,8 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Auth from './auth/Auth';
 import Home from './home/Home';
+import Publications from './publications/Publications';
+import Publication from './publications/publication/Publication';
 import Dashboard from './dashboard/Dashboard';
 import ProfileInfoEdit from './profileInfoEdit/ProfileInfoEdit';
 
@@ -18,6 +20,8 @@ const App = () => {
             <Switch>
                 <Route path="/" exact component={Home}/>
                 <Route path="/auth" exact component={Auth}/>
+                <Route path="/publications/team/:teamId" exact component={Publications}/>
+                <Route path="/publications/:pubId"exact component={Publication}/>
                 <Route path="/dashboard" exact component={Dashboard}/>
                 <Route path="/dashboard/profile" exact component={ProfileInfoEdit} />
 
