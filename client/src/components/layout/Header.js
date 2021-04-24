d73e16764151089aaed8dcaf19555e5113dccea5
Kunj Dave <kdav0006@student.monash.edu>
2021-04-24 16:20:36 +1000

    Add hard-coded ids and fetch username from redux store

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,28 +1,31 @@
 import React from "react"
 import { Navbar, Nav, Image } from "react-bootstrap"
 import { Link } from 'react-router-dom'
+import {useSelector} from 'react-redux';
 
 import "./Header.css"
 
 import profilePicture from "./user.png"
 
 /**
- * This functio provides header for Layout.js
+ * This function provides header for Layout.js
  * @returns Header component to be rendered in Layout.js
  */
 function Header() {
+    const userName = useSelector(state => state.user?.givenName + " " + state.user?.familyName);
+    //TODO: Remove hard-coded team id and publications id from the links
     return (
         <Navbar bg="primary" variant="dark" className="header">
             <Navbar.Brand><Link className="header-brand" to="/dashboard">Researchify</Link></Navbar.Brand>
             <Nav className="mr-auto">
                 <Nav.Link><Link className="header-link" to="/dashboard/profile">Profile</Link></Nav.Link>
-                <Nav.Link><Link className="header-link" to="/publications/team/:teamId">Publication Team</Link></Nav.Link>
-                <Nav.Link><Link className="header-link" to="/publications/:pubId">Publications</Link></Nav.Link>
+                <Nav.Link><Link className="header-link" to="/publications/team/606bb59c22201f529db920c9">Publication Team</Link></Nav.Link>
+                <Nav.Link><Link className="header-link" to="/publications/606e88366a6daf816da6ace5">Publications</Link></Nav.Link>
             </Nav>
             <Nav>
                 <Nav.Link className="header-username">
                     <Link className="header-link" to="/dashboard/profile">
-                        <Image className="profile-picture" src={profilePicture} roundedCircle /> Username
+                        <Image className="profile-picture" src={profilePicture} roundedCircle /> {userName}
                     </Link>
                 </Nav.Link>
             </Nav>
