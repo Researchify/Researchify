dfaf5dc8d0bb1620d4828b2b4eb43885b4655ab0
kdav <63642262+kdav108@users.noreply.github.com>
2021-04-25 12:46:37 +1000

    Add Login UI and Register UI with connection to database to add users (#42)
    * Add user collection with data in database
    
    * Added basic UI for registration
    
    * Added initial code for login & register backend
    
    * Refactor login and register backend
    
    * Altered UI of registration page
    
    * Deleted test Register component
    
    * Added navigation bar and functionality to record state changes from user inputs
    
    * Add update user details request
    
    * Added UI to go to login page
    
    * Refactored some code from class to function components
    
    * Fixed error: Didn't call super of class
    
    * Convert class components to functional components
    
    * Remove props
    
    * Refactored so that all user registration inputs are inserted to states of the same component
    
    * Added basic validation
    
    * Removed merge issues
    
    * Fixed more merge issues
    
    * More issues
    
    * more issues
    
    * more issues
    
    * Added UI for user login page.
    
    * Resolve merge conflict
    
    * Add action, api and reducer for adding user data
    
    * Only post to api when all fields are correct
    
    * Corrected href value from login to register to link the 2 pages properly.
    
    * Changed code for implementing validation.
    
    * Test
    
    * Use name instead of familyName and givenName in payload
    
    * Add initial dashboard page and make RegistrationForm save data to redux global store
    
    * Fix comments
    
    * Stop browser from reloading page upon signing in
    
    * Updated schema to now take given name and family name
    
    * Make redux store givenName and familyName
    
    * Update doc
    
    * Restructure file hierarchy
    
    * Fix merge issues
    
    * Remove ESLint errors
    
    * Applier formatter
    
    * Fix minor bugs relating to response codes used
    
    * Add GET request to retrieve user by ID
    
    * Remove and update duplicate file
    
    * Update response code
    
    * Restructure files and move NavBar one level up
    
    * Fix doc
    
    * Merge api/publicationns.js and api/userAPI.js
    
    * Restore publication schema
    
    * Add header to all pages
    
    * Add hard-coded ids and fetch username from redux store
    
    * Remove temporarily created NavBar
    
    * Move user.png into images folder
    
    Co-authored-by: Sharan Sharabinth <ssha0054@student.monash.edu>
    Co-authored-by: Akash Saggar <akasaggar@gmail.com>
    Co-authored-by: An Cao <acao0002@student.monash.edu>
    Co-authored-by: Akash Saggar <asag0003@student.monash.edu>
    Co-authored-by: ssha0054 <80957318+ssha0054@users.noreply.github.com>
    Co-authored-by: andrwcao <80874259+andrwcao@users.noreply.github.com>
    Co-authored-by: rsal0007 <rsal0007@student.monash.edu>
    Co-authored-by: vgoh1 <40584352+vgoh1@users.noreply.github.com>

---
+++ client/src/actions/users.js
@@ -0,0 +1,31 @@
+/**
+ * This file houses our user-related Action Creators.
+ */
+import * as api from "../api";
+import { ADD_USER_DATA } from "./types";
+
+/**
+ * This action creator will be called when a user registers.
+ *
+ * @param userData object containing user email, given and family name and password
+ * @returns a thunk responsible for posting the data to the api and dispatching
+ */
+export const addUserAction = (userData) => async (dispatch) => {
+  try {
+    const message = await api.addUserData(userData);
+    console.log("API responded with: ");
+    console.log(message);
+
+    // We do not need to store the password, so create a new object without it and then dispatch
+    const data = {
+      email: userData.email,
+      givenName: userData.givenName,
+      familyName: userData.familyName
+    };
+
+    // Store user related data in our redux global store
+    dispatch({ type: ADD_USER_DATA, payload: data });
+  } catch (err) {
+    console.error(`Error in posting user data to the api: ${err}`);
+  }
+};
