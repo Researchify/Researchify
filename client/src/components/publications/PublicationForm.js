082013c340a8e31eb71c1bfa1a390357d76be728
mable-yip <47068733+mable-yip@users.noreply.github.com>
2021-05-04 18:51:53 +1000

    Commit partial work

--- client/src/components/publications/PublicationForm.js
+++ client/src/components/publications/PublicationForm.js
@@ -1,237 +1,183 @@
-import React, {useState} from 'react'
-import { Button, Form, FormGroup, Tooltip, OverlayTrigger, Row, InputGroup } from 'react-bootstrap'
-import { useDispatch } from 'react-redux';
+import { Formik, FieldArray, Field, ErrorMessage, Form } from "formik";
+import * as yup from "yup";
+import { Row, InputGroup, Button, Tooltip, OverlayTrigger} from "react-bootstrap";
 import { updatePublication, createPublication } from '../../actions/publications'
-import { Formik, FieldArray, Field } from 'formik'
-import * as Yup from 'yup';
+import { useDispatch } from 'react-redux';
 
-
-const validateSchema = Yup.object({
-    title: Yup.string()
-        .min(3, 'Must be 3 characters or more')
-        .required('Title is required'),
-    yearPublished: Yup.string(),
-    description: Yup.string()
-        .min(5, 'Must be 5 characters or more')
-        .required('Description is required'),
-    authors: Yup.string()
-        .min(6, 'Password must be at least 6 charaters')
-        .required('Password is required'),
-    link: Yup.string()
-        .url('Please provide a vaild url')
-        .required('Link is required'),
+const schema = yup.object({
+    title: yup.string().required().min(3, "at least 3 characters"),
+    yearPublished: yup.string().required(),
+    authors: yup.array().of(yup.string().required("author can't be empty")), 
+    description: yup.string().required().min(5, "at least 5 characters"),
+    link: yup.string().required().url("Please provide a valid url"),
   })
 
 const initialValues = {
     title: "",
-    description: "",
+    yearPublished: (new Date()).getFullYear().toString(),
     authors: [""],
-    yearPublished: "",
+    description: "",
     link: "",
     teamId: "606bb59c22201f529db920c9" // teamId should be get from redux state later
 }
 
-const onSubmit = (values, submitProps) => {
-    console.log('Form data', values)
-    console.log('submitProps', submitProps)
-    submitProps.setSubmitting(false)
-    submitProps.resetForm()
-  }
-
 
 const PublicationForm = (props) => {
+
+    const dispatch = useDispatch()
     const year = (new Date()).getFullYear();
     const years = Array.from(new Array(year-1899),( val, index) => year - index);
-    const [formValues, setFormValues] = useState(props.type === "update" ? props.pub : initialValues)
-
+    
     const renderTooltip = (props) => (
         <Tooltip id="button-tooltip" {...props}>
             You will lose your progress
         </Tooltip>
     )
 
-    console.log(formValues)
+    const onSubmit = (values) => {
+        console.log(values)
+        if (props.type === "update"){
+            dispatch(updatePublication(props.pub._id, values))
+        } else if (props.type === "create"){
+            dispatch(createPublication(values))
+        }
+        props.closeModal() 
+    }
 
-    return (
-        <Formik
-            initialValues={formValues}
-            validationSchema={validateSchema}
-            onSubmit={onSubmit}
-        >
-            {({
-                handleChange,
-                values,
-                touched,
-                errors,
-        }) => (
-            <Form>
-                <Form.Group>
-                    <Form.Label>Title</Form.Label>
-                    <Form.Control
-                        as="textarea" 
-                        rows={2}
-                        name="title"
-                        placeholder="Title" 
-                        value={values.title}
-                        onChange={handleChange}
-                        isInvalid={touched.title && errors.title}
-                    />
-                    <Form.Control.Feedback  type="invalid">{errors.title}</Form.Control.Feedback>
-                </Form.Group>
+    return(
+        <>
+            <Formik
+                validationSchema={schema}
+                onSubmit={(values)=> onSubmit(values)}
+                initialValues={props.type === "update" ? props.pub : initialValues}
+            
+            >
+                {({ handleSubmit, handleChange, values, touched, errors }) => 
+                (
 
-                <Form.Group>
-                    <Form.Label>Year Published</Form.Label>
-                    <Form.Control
-                        as="select"
-                        type="text"
-                        name="yearPublished"
-                        value={values.yearPublished}
-                        onChange={handleChange}
-                    >
-                    {
-                        years.map((year, index) => {
-                            return <option key={`year${index}`} value={year}>{year}</option>
-                        })
-                    }
-                    </Form.Control>
-                </Form.Group>
-                
-                <Form.Group>
-                    <Form.Label>Description</Form.Label>
-                    <Form.Control
-                        as="textarea" 
-                        rows={4}
-                        name="description"
-                        placeholder="Description" 
-                        value={values.description}
-                        onChange={handleChange}
-                        isInvalid={touched.description && errors.description}
-                    />
-                    <Form.Control.Feedback  type="invalid">{errors.description}</Form.Control.Feedback>
-                </Form.Group>
+                    <Form noValidate onSubmit={handleSubmit}>
 
-                <FormGroup>
-                    <Form.Label>Authors </Form.Label>
-                        <FieldArray name='authors'>
-                            {fieldArrayProps => {
-                            const { push, remove, form } = fieldArrayProps
-                            const { values } = form
-                            const { authors } = values
-                            return (
-                                <div>
-                                {authors.map((authors, index) => (
-                                    <div key={index}>
-                                        <InputGroup>
-                                            <Form.Control name={`authors[${index}]`} />
+                        <Form.Group>
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control
+                                    as="textarea"
+                                    row={2}
+                                    name="title"
+                                    placeholder="Title"
+                                    value={values.title}
+                                    onChange={handleChange}
+                                    isInvalid={touched.title && errors.title}
+                            />
+                            <Form.Control.Feedback type="invalid">{errors.title}</Form.Control.Feedback>
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label>Published Year</Form.Label>
+                            <Form.Control
+                                as="select"
+                                type="text"
+                                name="yearPublished"
+                                value={values.yearPublished}
+                                onChange={handleChange}
+                            >
+                            {
+                                years.map((year, index) => {
+                                    return <option key={`year${index}`} value={year}>{year}</option>
+                                })
+                            }
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group>
+                        <Form.Label>Authors </Form.Label>
+                        <FieldArray name='authors' className="form-control">
+                            {
+                                fieldArrayProps => {
+                                const { push, remove, form } = fieldArrayProps
+                                const { values } = form
+                                const { authors } = values
+                                return (
+                                    <div>
+                                        {
+                                            authors.map((author, index) => (
+                                            <InputGroup key={index}>
+                                                <Field name={`authors[${index}]`} required/>
                                                 <InputGroup.Append>
-                                                    <Button disabled={values.authors.length === 1} variant="outline-secondary"  onClick={() => remove(index)}>
+                                                    <Button variant="outline-secondary" disabled={authors.length === 1} onClick={() => remove(index)}>
                                                         Remove
                                                     </Button>
                                                 </InputGroup.Append>
-                                        </InputGroup>
+                                            </InputGroup>
+                                    ))}
+                                    <Form.Control
+                                        isInvalid={touched.authors && errors.authors}
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        {errors.authors}
+                                    </Form.Control.Feedback>
+
+                                    <div> {errors.authors} </div>
+                                    <Button  variant="secondary" className="mt-2" onClick={() => push('')}>
+                                        Add Author
+                                    </Button>
                                     </div>
-                                ))}
-                                <Button 
-                                    variant="secondary" 
-                                    onClick={() => push('')}
-                                > 
-                                    Add Author 
-                                    </Button>  
-                                </div>
-                            )
-                            }}
+                                )}
+                            }
+                            
                         </FieldArray>
-                <InputGroup>
-   
-                </InputGroup>
-                </FormGroup>
-                
-                <Form.Group>
-                    <Form.Label>Link</Form.Label>
-                    <Form.Control
-                        type="text"
-                        name="link"
-                        placeholder="Link" 
-                        value={values.link}
-                        onChange={handleChange}
-                        isInvalid={touched.link && errors.link}
-                    />
-                    <Form.Control.Feedback type="invalid">{errors.link}</Form.Control.Feedback>
-                </Form.Group>
+                        </Form.Group>
 
-                 <Row>
-                    <div className="ml-auto mr-3">
-                        <OverlayTrigger
-                            trigger={["hover", "focus"]}
-                            placement="bottom"
-                            overlay={renderTooltip}
-                        >
-                            <Button className="mr-2" variant="outline-danger" onClick={props.closeModal}>
-                                Cancel
-                            </Button>
-                        </OverlayTrigger>
-                        <Button type='submit' variant="primary"> Confirm </Button>
-                    </div>
-                </Row>
-            </Form>
-        )}
+                        <Form.Group>
+                            <Form.Label>Description</Form.Label>
+                            <Form.Control
+                                as="textarea"
+                                row={3}     // why not working????
+                                name="description"
+                                placeholder="Description"
+                                value={values.description}
+                                onChange={handleChange}
+                                isInvalid={touched.description && errors.description}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.description}
+                            </Form.Control.Feedback>
+                        </Form.Group>
 
-        </Formik>
 
-    // const handleSubmit = (event) => {
-    //     const form = event.currentTarget;
-    //     if (form.checkValidity() === false) {
-    //       event.preventDefault()
-    //       event.stopPropagation()
-    //     } else{
-    //         if (props.type === "update"){
-    //             dispatch(updatePublication(props.pub._id, formInput))
-    //         } else if (props.type === "create"){
-    //             dispatch(createPublication(formInput))
-    //         }
-    //         props.closeModal()    
-    //     }
-    //     setValidated(true)
-    //     event.preventDefault()
-    // }
+                        <Form.Group>
+                            <Form.Label>link</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name="link"
+                                    placeholder="link"
+                                    value={values.link}
+                                    onChange={handleChange}
+                                    isInvalid={touched.link && errors.link}
+                                />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.link}
+                            </Form.Control.Feedback>
+                        </Form.Group>
 
-    // const renderAuthors = () =>{
-    //     return(
-    //         formInput.authors.map((author, index) => 
-    //             <InputGroup key={index}>
-    //                 <Form.Control
-    //                     placeholder="Author"
-    //                     required
-    //                     minLength="1"
-    //                     type="text"
-    //                     value={formInput.authors[index]}
-    //                     onChange={(event) => {
-    //                         let newAuthors = formInput.authors
-    //                         newAuthors[index] = event.target.value
-    //                         setFormInput({...formInput, authors: newAuthors})
-    //                     }}
-    //                 />
-    //                 <InputGroup.Append>
-    //                     <Button 
-    //                         disabled={formInput.authors.length === 1}
-    //                         variant="outline-secondary" 
-    //                         onClick={() => {
-    //                             let newAuthors = formInput.authors
-    //                             newAuthors.splice(index, 1)
-    //                             setFormInput({...formInput, authors: newAuthors})
-    //                         }}
-    //                     >
-    //                         Remove
-    //                     </Button>
-    //                 </InputGroup.Append>
-    //                 <Form.Control.Feedback type="invalid">
-    //                     Authors must not be empty
-    //                 </Form.Control.Feedback>
-    //             </InputGroup>
-    //         )
-    //     )
-    // }
+                        <Row>
+                            <div className="ml-auto mr-3">
+                                <OverlayTrigger
+                                    trigger={["hover", "focus"]}
+                                    placement="bottom"
+                                    overlay={renderTooltip}
+                                >
+                                    <Button className="mr-2" variant="outline-danger" onClick={props.closeModal}>
+                                        Cancel
+                                    </Button>
+                                </OverlayTrigger>
+                                <Button type="submit">Confirm</Button>
+                            </div>
+                        </Row>
+                    </Form>
+                )}
+            </Formik>
+        </>
     )
 }
 
-export default PublicationForm 
+export default PublicationForm
