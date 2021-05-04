ff8ced2688ea8f87cbd48c835303b4aac83b6df2
mable-yip <47068733+mable-yip@users.noreply.github.com>
2021-05-04 22:33:15 +1000

    Refractor publication form to use formilk

--- client/src/components/publications/PublicationForm.js
+++ client/src/components/publications/PublicationForm.js
@@ -1,15 +1,15 @@
-import { Formik, FieldArray, Field, ErrorMessage, Form } from "formik";
+import { Formik } from "formik";
 import * as yup from "yup";
-import { Row, InputGroup, Button, Tooltip, OverlayTrigger} from "react-bootstrap";
+import { Row, InputGroup, Button, Tooltip, OverlayTrigger, Form} from "react-bootstrap";
 import { updatePublication, createPublication } from '../../actions/publications'
 import { useDispatch } from 'react-redux';
 
 const schema = yup.object({
-    title: yup.string().required().min(3, "at least 3 characters"),
+    title: yup.string().required("Title is required").min(3, "Title must be at least 3 characters"),
     yearPublished: yup.string().required(),
-    authors: yup.array().of(yup.string().required("author can't be empty")), 
-    description: yup.string().required().min(5, "at least 5 characters"),
-    link: yup.string().required().url("Please provide a valid url"),
+    authors: yup.array().of(yup.string().required("Authors must not be empty")).required("Author is required"), 
+    description: yup.string().required("Description is required").min(5, "Description must be at least 5 characters"),
+    link: yup.string().required("Link is required").url("Link URL provided is not a valid URL, including the protocol (http/https)"),
   })
 
 const initialValues = {
@@ -21,9 +21,7 @@ const initialValues = {
     teamId: "606bb59c22201f529db920c9" // teamId should be get from redux state later
 }
 
-
 const PublicationForm = (props) => {
-
     const dispatch = useDispatch()
     const year = (new Date()).getFullYear();
     const years = Array.from(new Array(year-1899),( val, index) => year - index);
@@ -44,29 +42,59 @@ const PublicationForm = (props) => {
         props.closeModal() 
     }
 
+    const renderAuthors = (values, touched, errors, handleChange, setValues) => {
+        return(
+            values.authors.map((author, index) => (
+                <InputGroup key={index}>
+                    <Form.Control 
+                        type="text"
+                        placeholder="Author"
+                        name={`authors[${index}]`} 
+                        value={values.authors[index]} 
+                        onChange={handleChange}
+                        isInvalid={touched.authors && errors.authors && errors.authors[index]}
+                    />
+                    <InputGroup.Append>
+                        <Button 
+                            onClick={()=>{
+                                let newAuthors = values.authors
+                                newAuthors.splice(index, 1)
+                                setValues({...values, authors: newAuthors})
+                            }}
+                            variant="outline-secondary" 
+                            disabled={values.authors.length === 1}
+                        >
+                            Remove
+                        </Button>
+                    </InputGroup.Append>  
+                    <Form.Control.Feedback type="invalid">{errors.authors && errors.authors[index]}</Form.Control.Feedback>  
+                </InputGroup>
+            ))         
+        )
+    }
+
     return(
         <>
             <Formik
+                enableReinitialize 
                 validationSchema={schema}
                 onSubmit={(values)=> onSubmit(values)}
                 initialValues={props.type === "update" ? props.pub : initialValues}
             
             >
-                {({ handleSubmit, handleChange, values, touched, errors }) => 
+                {({ handleSubmit, handleChange, values, touched, errors, setValues }) => 
                 (
-
                     <Form noValidate onSubmit={handleSubmit}>
-
                         <Form.Group>
                             <Form.Label>Title</Form.Label>
                             <Form.Control
-                                    as="textarea"
-                                    row={2}
-                                    name="title"
-                                    placeholder="Title"
-                                    value={values.title}
-                                    onChange={handleChange}
-                                    isInvalid={touched.title && errors.title}
+                                as="textarea"
+                                row={2}
+                                name="title"
+                                placeholder="Title"
+                                value={values.title}
+                                onChange={handleChange}
+                                isInvalid={touched.title && errors.title}
                             />
                             <Form.Control.Feedback type="invalid">{errors.title}</Form.Control.Feedback>
                         </Form.Group>
@@ -89,60 +117,32 @@ const PublicationForm = (props) => {
                         </Form.Group>
 
                         <Form.Group>
-                        <Form.Label>Authors </Form.Label>
-                        <FieldArray name='authors' className="form-control">
-                            {
-                                fieldArrayProps => {
-                                const { push, remove, form } = fieldArrayProps
-                                const { values } = form
-                                const { authors } = values
-                                return (
-                                    <div>
-                                        {
-                                            authors.map((author, index) => (
-                                            <InputGroup key={index}>
-                                                <Field name={`authors[${index}]`} required/>
-                                                <InputGroup.Append>
-                                                    <Button variant="outline-secondary" disabled={authors.length === 1} onClick={() => remove(index)}>
-                                                        Remove
-                                                    </Button>
-                                                </InputGroup.Append>
-                                            </InputGroup>
-                                    ))}
-                                    <Form.Control
-                                        isInvalid={touched.authors && errors.authors}
-                                    />
-                                    <Form.Control.Feedback type="invalid">
-                                        {errors.authors}
-                                    </Form.Control.Feedback>
+                            <Form.Label>Authors </Form.Label>
+                            { renderAuthors(values, touched, errors, handleChange, setValues) }
 
-                                    <div> {errors.authors} </div>
-                                    <Button  variant="secondary" className="mt-2" onClick={() => push('')}>
-                                        Add Author
-                                    </Button>
-                                    </div>
-                                )}
-                            }
-                            
-                        </FieldArray>
-                        </Form.Group>
+                            <Button 
+                                variant="secondary" 
+                                onClick={()=> {
+                                setValues({...values, authors: [...values.authors, ""]})
+                                }
+                            }>
+                                Add Author
+                            </Button>
+                        </Form.Group>    
 
                         <Form.Group>
                             <Form.Label>Description</Form.Label>
                             <Form.Control
                                 as="textarea"
-                                row={3}     // why not working????
+                                row={4} 
                                 name="description"
                                 placeholder="Description"
                                 value={values.description}
                                 onChange={handleChange}
                                 isInvalid={touched.description && errors.description}
                             />
-                            <Form.Control.Feedback type="invalid">
-                                {errors.description}
-                            </Form.Control.Feedback>
+                            <Form.Control.Feedback type="invalid">{errors.description}</Form.Control.Feedback>
                         </Form.Group>
-
 
                         <Form.Group>
                             <Form.Label>link</Form.Label>
@@ -154,9 +154,7 @@ const PublicationForm = (props) => {
                                     onChange={handleChange}
                                     isInvalid={touched.link && errors.link}
                                 />
-                            <Form.Control.Feedback type="invalid">
-                                {errors.link}
-                            </Form.Control.Feedback>
+                            <Form.Control.Feedback type="invalid">{errors.link}</Form.Control.Feedback>
                         </Form.Group>
 
                         <Row>
