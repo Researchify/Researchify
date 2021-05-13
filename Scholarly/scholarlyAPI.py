771040a103aa80a329b7fecc85e37f761a3b8bb2
Raj <rsun0013@student.monash.edu>
2021-05-13 15:33:05 +1000

    make a separate dir for scholarly stuff and dockerfile for aws integration

--- Scholarly/scholarlyAPI.py
+++ Scholarly/scholarlyAPI.py
@@ -3,11 +3,13 @@ from scholarly import scholarly
 def getPublications(author):
     search_query = scholarly.search_author(author)
     authorObject = scholarly.fill(next(search_query))
-    print(scholarly.fill(authorObject['publications'][0])['bib']['title'])
+    print(scholarly.fill(authorObject['publications'][0])['bib']['author'])
     publications = []
     for pub in authorObject['publications']:
-        publications.append(scholarly.fill(pub))
+        currentPub = scholarly.fill(pub)
+        publications.append(currentPub)
 
     for i in publications:
         print(i)
+    return publications
 getPublications("Gleb belov")
