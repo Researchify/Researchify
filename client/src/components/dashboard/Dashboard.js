0f9d73945746eefa02db408403820e8d1db40c38
Jie Fan <jfan0010@student.monash.edu>
2021-05-14 22:42:41 +1000

    Applied redux hooks in profileInfoEdit.js

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -7,28 +7,7 @@ const Dashboard = () => {
     return (
         <main>                
             <Container fluid>
-                <p> 
-                <br></br><br></br><br></br><br></br>
                 You are currently viewing dashboard page. 
-                <br></br><br></br><br></br><br></br>
-                This will be a page full of br to test sticky sidebar.
-                <br></br><br></br><br></br><br></br>
-                <br></br><br></br><br></br><br></br>
-                <br></br><br></br><br></br><br></br>
-                <br></br><br></br><br></br><br></br>
-                <br></br><br></br><br></br><br></br>
-                <br></br><br></br><br></br><br></br>
-                <br></br><br></br><br></br><br></br>
-                <br></br><br></br><br></br><br></br>
-                <br></br><br></br><br></br><br></br>
-                <br></br><br></br><br></br><br></br>
-                <br></br><br></br><br></br><br></br>
-                <br></br><br></br><br></br><br></br>
-                <br></br><br></br><br></br><br></br>
-                <br></br><br></br><br></br><br></br>
-                <br></br><br></br><br></br><br></br>
-                <br></br><br></br><br></br><br></br>
-                </p>
             </Container>
         </main>
     )
