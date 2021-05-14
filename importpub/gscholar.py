7ccd586911d9da7b00d04e4a6fd6529620d4f308
vgoh1 <vgoh0001@student.monash.edu>
2021-05-15 09:52:07 +1000

    Use logging module to log debug info, refactoring based on PFB

--- importpub/gscholar.py
+++ importpub/gscholar.py
@@ -1,20 +1,23 @@
+"""
+This file contains the functions relating to getting publications from google scholar.
+"""
 from scholarly import scholarly
 import json
 
-def getPublications(author_id):
+def get_publications(author_id):
     """
     This function uses the scholarly library to retrieve publications associated with an author.
     :param author_id: google scholar user id extracted from their profile url
     :return: json string of publications in a list
     """
     search_query = scholarly.search_author_id(author_id)
-    authorObject = scholarly.fill(search_query, publication_limit=10)
+    author_object = scholarly.fill(search_query, publication_limit=10)
     pub_list = []
 
-    for i in range(len(authorObject["publications"])):
-        currentPub = scholarly.fill(authorObject["publications"][i])
-        del currentPub["source"]
-        pub_list.append(currentPub)
+    for pub in author_object["publications"]:
+        current_pub = scholarly.fill(pub)
+        del current_pub["source"]
+        pub_list.append(current_pub)
 
     pub_json = json.dumps({ "publications": pub_list })
     return pub_json
--- importpub/handler.py
+++ importpub/handler.py
@@ -1,4 +1,19 @@
-from gscholar import getPublications 
+"""
+This file contains the AWS lambda handler for importing publications from google scholar.
+"""
+from gscholar import get_publications 
+import logging
+import sys
+
+def enable_logging():
+    """
+    This function sets up the logging module to log to stdout.
+    """
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    root.addHandler(handler)
 
 def lambda_handler(event,context):
     """
@@ -6,8 +21,9 @@ def lambda_handler(event,context):
     :param event: a json object containing the author_id
     :param context: object with context properties see: https://docs.aws.amazon.com/lambda/latest/dg/python-context.html
     """
+    enable_logging()
     author_id = event["author_id"]
-    print(author_id)
-    return getPublications(author_id) 
+    logging.debug(author_id)
+    return get_publications(author_id) 
 
 
