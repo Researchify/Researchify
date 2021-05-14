4e005619926dc7aa03c3c28001e93ac0206b1e30
Sharan Sharabinth <ssha0054@student.monash.edu>
2021-05-14 14:00:32 +1000

    Modify User model to store teamName  and orgName

--- api/src/models/user.model.js
+++ api/src/models/user.model.js
@@ -5,8 +5,8 @@ const mongoose = require('mongoose');
 
 
 const userSchema = new mongoose.Schema({
-    givenName: { type: String, required: false },
-    familyName: { type: String, required: false },
+    teamName: { type: String, required: true },
+    orgName: { type: String, required: false },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
 }, {timestamps: true})
