014411cc6ccd8c395d4abff5d2aa56e5607f9997
vgoh1 <vgoh0001@student.monash.edu>
2021-05-14 14:20:55 +1000

    Update dockerfile to use aws lambda image, update function to return json

--- Scholarly/scholarlyAPI.py
+++ Scholarly/scholarlyAPI.py
@@ -1,15 +1,24 @@
 from scholarly import scholarly
+import json
 
-def getPublications(author):
-    search_query = scholarly.search_author(author)
-    authorObject = scholarly.fill(next(search_query))
-    print(scholarly.fill(authorObject['publications'][0])['bib']['author'])
-    publications = []
-    for pub in authorObject['publications']:
-        currentPub = scholarly.fill(pub)
-        publications.append(currentPub)
+def lambda_handler(event,context):
+    author = event["author"]
+    print(author)
+    return getPublications(author) 
 
-    for i in publications:
-        print(i)
-    return publications
-getPublications("Gleb belov")
+def getPublications(author_id):
+    # author is going to be id
+    search_query = scholarly.search_author_id(author_id)
+    # print("one")
+    authorObject = scholarly.fill(search_query, publication_limit=10)
+    # print("two")
+    pub_list = []
+
+    for i in range(len(authorObject["publications"])):
+        currentPub = scholarly.fill(authorObject["publications"][i])
+        del currentPub["source"]
+        pub_list.append(currentPub)
+
+    pub_json = json.dumps({ "publications": pub_list })
+    # print(pub_json)
+    return pub_json
