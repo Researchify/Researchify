87da0c828f1b84aa790850e91dd82260d11a4260
rpra0006 <53892953+rpra0006@users.noreply.github.com>
2021-05-20 14:52:13 +0700

    Revert "Merge logiciel to Editor"

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,6 +8,7 @@ import Auth from './auth/Auth';
 import Home from './home/Home';
 import Dashboard from './dashboard/Dashboard';
 import ProfileInfoEdit from './profileInfoEdit/ProfileInfoEdit';
+import Editor from './editor/Editor'
 
 import Register from './auth/Register';
 import Login from './auth/Login';
@@ -15,15 +16,25 @@ import Layout from './layout/Layout';
 import SidebarData from './layout/SidebarData'
 
 import './layout/Layout.css'
-
 import PublicationPage from './publications/PublicationPage';
-import { Fragment } from 'react';
 
 const App = () => {
     const urls={
         dashboard: "/dashboard",
-        profile: "/dashboard/profile"
+        profile: "/dashboard/profile",
+        editor: "/editor",
     }
+
+    const headerData = [
+        {
+            title:"Profile",
+            link: "/dashboard/profile"
+        },
+        {
+            title:"Publications",
+            link: "/publications/team/606bb59c22201f529db920c9"
+        }      
+    ]
     return (
         <BrowserRouter>
             <Switch>
@@ -31,24 +42,16 @@ const App = () => {
                 <Route path="/auth" exact component={Auth} />
                 <Route path="/register" exact component={Register} />
                 <Route path="/login" exact component={Login} />
-                <Fragment>
-                    <Header title={"Researchify"} urls={urls}/>      
-                    <Container fluid>
-                        <Row>
-                        <Col className="sidebar-wrapper" md={2} lg={2} xl={1}>
-                            <Sidebar />
-                        </Col>
-                        <Col className="page-content-wrapper" md={10} lg={10} xl={11}>
-                            <Route path="/publications/team" exact component={PublicationPage}/>
-                            <Route path={urls.dashboard} exact component={Dashboard} />
-                            <Route path="/dashboard/profile" exact component={ProfileInfoEdit} />
-                        </Col>
-                        </Row>
-                    </Container>
-                </Fragment>                
+                <Route path="/editor" exact component={Editor}/>
+                {console.log(headerData)}
+                <Layout sidebarData={SidebarData} headerData={headerData}>
+                    <Route path="/publications/team/:teamId" exact component={PublicationPage}/>
+                    <Route path={urls.dashboard} exact component={Dashboard} />
+                    <Route path="/dashboard/profile" exact component={ProfileInfoEdit} />
+        
+                </Layout>
             </Switch>             
         </BrowserRouter>
     );
 };
-
 export default App;
