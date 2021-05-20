b225d6c11ed6894c3ab59f705eb937e34d078016
mable-yip <47068733+mable-yip@users.noreply.github.com>
2021-05-20 20:18:39 +1000

    Improve CSS styling

--- client/src/components/publications/Publications.js
+++ client/src/components/publications/Publications.js
@@ -50,7 +50,7 @@ const Publications = () => {
 
     return (
         <> 
-            <div className="mb-3 mt-3 text-center">
+            <div className="mb-3 mt-4 text-center">
                 <Button className="mr-2" onClick={() => setShowCreateForm(true)}>    
                     Add Publication
                 </Button>
@@ -58,32 +58,29 @@ const Publications = () => {
                     Import Publication
                 </Button>
             </div>
-
-            <Row>
-                <Col xs={{span: 3, offset: 9}}>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="light" className="mb-2">
-                            Layout: {layout}
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            {
-                                Object.keys(allLayouts).map(layout => 
-                                    <Dropdown.Item as="button"onClick={()=>setLayout(allLayouts[layout])}>
-                                        {allLayouts[layout]}
-                                    </Dropdown.Item>
-                                )
-                            }
-                        </Dropdown.Menu>
-                    </Dropdown>
-                        
-                    <DropdownButton variant="light" id="dropdown-item-button" title={"Sort by: "+sortingOption} >
-                        <Dropdown.Item as="button" value="Year" onClick={e => {dispatch(sortPublications(teamPublications, e.target.value)); setSortingOption(e.target.value)}}>Year</Dropdown.Item>
-                        <Dropdown.Item as="button" value="Author" onClick={e => {dispatch(sortPublications(teamPublications, e.target.value)); setSortingOption(e.target.value)}}>Author</Dropdown.Item>
-                        <Dropdown.Item as="button" value="Title" onClick={e => {dispatch(sortPublications(teamPublications, e.target.value)); setSortingOption(e.target.value)}}>Title</Dropdown.Item>
-                        { toggleSortingOptions() }
-                    </DropdownButton>
-                </Col>
-            </Row>
+            <div className="mb-3 mt-3 text-center">
+                        <Dropdown className="ml-5">
+                            <Dropdown.Toggle variant="light" className="mb-2">
+                                Layout: {layout}
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                {
+                                    Object.keys(allLayouts).map(layout => 
+                                        <Dropdown.Item as="button"onClick={()=>setLayout(allLayouts[layout])}>
+                                            {allLayouts[layout]}
+                                        </Dropdown.Item>
+                                    )
+                                }
+                            </Dropdown.Menu>
+                        </Dropdown>
+                            
+                        <DropdownButton className="ml-4" variant="light" id="dropdown-item-button" title={"Sort by: "+sortingOption} >
+                            <Dropdown.Item as="button" value="Year" onClick={e => {dispatch(sortPublications(teamPublications, e.target.value)); setSortingOption(e.target.value)}}>Year</Dropdown.Item>
+                            <Dropdown.Item as="button" value="Author" onClick={e => {dispatch(sortPublications(teamPublications, e.target.value)); setSortingOption(e.target.value)}}>Author</Dropdown.Item>
+                            <Dropdown.Item as="button" value="Title" onClick={e => {dispatch(sortPublications(teamPublications, e.target.value)); setSortingOption(e.target.value)}}>Title</Dropdown.Item>
+                            { toggleSortingOptions() }
+                        </DropdownButton>
+            </div>
 
             <div className="text-center">
                 {
