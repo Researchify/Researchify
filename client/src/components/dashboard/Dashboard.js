0c471b9d0654fa37aade603b7f9c69e5a45abe1e
Jie Fan <jfan0010@student.monash.edu>
2021-05-22 01:51:25 +1000

    Apply Redux for website creation card in Dashboard.js

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,9 @@
-import { Container, CardGroup, Card, Button } from 'react-bootstrap';
+/** Redux **/
+import { useDispatch, useSelector } from 'react-redux';
+import { createWebsite } from '../../actions/website';
+
+import { Link } from 'react-router-dom';
+import { Container, CardGroup, Card, Button, } from 'react-bootstrap';
 /** icons **/
 import { BsPencilSquare, BsServer, BsDisplayFill } from 'react-icons/bs'
 /** css **/
@@ -6,13 +11,18 @@ import './Dashboard.css'
 
 
 const Dashboard = () => {
+    const dispatch = useDispatch();
+    const websiteIsCreated = useSelector(state => state.website.isCreated);
+
     return (
 
-        <Container fluid>
+        <Container fluid className="researchify-dashboard-container">
             <Card className="text-center researchify-dashboard-card">
 
                 <Card.Body>
-                    <Button>Build a new Website</Button>
+                    <Button onClick={() => dispatch(createWebsite())}>
+                        {websiteIsCreated ? "Build a new Website" : "Edit the Website"}
+                    </Button>
                 </Card.Body>
 
                 <Card.Body className="researchify-dashboard-card-description">
@@ -22,25 +32,31 @@ const Dashboard = () => {
                 {/* Bottom layer of the three icons */}
                 <CardGroup className="researchify-dashboard-card-group">
                     <Card>
-                        <Card.Body>
-                            <BsPencilSquare className="researchify-dashboard-card-icons" />
-                        </Card.Body>
-                        <p>Editor</p>
+                        <Link className="researchify-dashboard-card-link">
+                            <Card.Body>
+                                <BsPencilSquare className="researchify-dashboard-card-icons" />
+                            </Card.Body>
+                            <p>Editor</p>
+                        </Link>
                     </Card>
                     <Card>
-                        <Card.Body>
-                            <BsServer className="researchify-dashboard-card-icons" />
-                        </Card.Body>
-                        <p>API Acess Manager</p>
+                        <Link className="researchify-dashboard-card-link">
+                            <Card.Body>
+                                <BsServer className="researchify-dashboard-card-icons" />
+                            </Card.Body>
+                            <p>API Acess Manager</p>
+                        </Link>
                     </Card>
                     <Card>
-                        <Card.Body>
-                            <BsDisplayFill className="researchify-dashboard-card-icons" />
-                        </Card.Body>
-                        <p>Website</p>
+                        <Link className="researchify-dashboard-card-link">
+                            <Card.Body>
+                                <BsDisplayFill className="researchify-dashboard-card-icons" />
+                            </Card.Body>
+                            <p>Website</p>
+                        </Link>
                     </Card>
                 </CardGroup>
-                
+
             </Card>
         </Container>
 
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -7,11 +7,13 @@ import authReducer from "./authReducer";
 import publicationsReducer from './publicationsReducer'
 import userReducer from "./usersReducer";
 import teamReducer from "./teamReducer";
+import websiteReducer from './websiteReducer';
 
 
 export default combineReducers({
     auth: authReducer,
     publications: publicationsReducer,
     user: userReducer,
-    team: teamReducer
+    team: teamReducer,
+    website: websiteReducer
 });
