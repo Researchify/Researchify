87229e4a3a068ad37eef1222cc7224c04b4b8ba7
rpra0006 <rpra0006@student.monash.edu>
2021-05-22 17:26:23 +0700

    Add different modal for website templates sample in Dashboard.js

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -11,22 +11,26 @@ import { createWebsite } from '../../actions/website';
 import { Link } from 'react-router-dom';
 
 /** icons **/
-import { BsPencilSquare, BsServer, BsDisplayFill } from 'react-icons/bs'
+import { BsPencilSquare, BsServer, BsDisplayFill, BsChevronRight, BsChevronLeft } from 'react-icons/bs'
 /** css **/
 import './Dashboard.css'
 // import DashboardModal from './DashboardModal'
-import homeTemplateImg from '../../images/home_page_template.jpg'
+import homeTemplateImg1 from '../../images/home_page_template1.jpg'
+import homeTemplateImg2 from '../../images/home_page_template2.jpg'
 
 const Dashboard = () => {
 
     const [show1, setShow1] = useState(false);
     const [show2, setShow2] = useState(false);
+    const [show3, setShow3] = useState(false);
 
     const handleCloseOne = () => setShow1(false);
-    const handleShowOne = () => setShow1(true);
-
     const handleCloseTwo = () => setShow2(false);
+    const handleCloseThree = () => setShow3(false);
+
+    const handleShowOne = () => setShow1(true);
     const handleShowTwo = () => setShow2(true);
+    const handleShowThree = () => setShow3(true);
 
 
     const dispatch = useDispatch();
@@ -74,54 +78,85 @@ const Dashboard = () => {
                         </Link>
                     </Card>
                 </CardGroup>
-
             </Card>
-            <Modal show={show1} onHide={handleCloseOne}  centered size="lg">
-                    <Modal.Header closeButton>
-                        <Modal.Title id="contained-modal-title-vcenter">
-                        Select a template
-                        </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <h4>Centered Modal</h4>
-                        <Image src={homeTemplateImg} className="img-fluid"/>
-                        <p>
-                        Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                        dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                        consectetur ac, vestibulum at eros. This is the first Model
-                        </p>
-                    </Modal.Body>
-                    
-                    <Modal.Footer>
-                        <Button onClick={() => {
-                        handleCloseOne();
-                        handleShowTwo()}}>
-                            Select
-                            </Button>
-                    </Modal.Footer>
-                </Modal>
 
-                <Modal show={show2} onHide={handleCloseTwo}  centered size="lg">
-                    <Modal.Header closeButton>
-                        <Modal.Title id="contained-modal-title-vcenter">
-                        Select a template
-                        </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <h4>Centered Modal</h4>
-                        <Image src={homeTemplateImg} className="img-fluid"/>
-                        <p>
-                        Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                        dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                        consectetur ac, vestibulum at eros. This is the second Modal
-                        </p>
-                    </Modal.Body>
-                    
-                    <Modal.Footer>
-                        <Button onClick={() => {dispatch(createWebsite());
-                        handleCloseTwo()}}>Select</Button>
-                    </Modal.Footer>
-                </Modal>
+            {'Entering User GitHub Pages Token'}
+            <Modal show={show1} onHide={handleCloseOne}  centered size="lg">
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                    Select a template
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <h4>Centered Modal</h4>
+                </Modal.Body>
+                
+                <Modal.Footer>
+                    <Button onClick={() => {
+                    handleCloseOne();
+                    handleShowTwo()}}>
+                        Next
+                        </Button>
+                </Modal.Footer>
+            </Modal>
+            
+            {'Selecting Template 1 Modal'}
+            <Modal show={show2} onHide={handleCloseTwo}  centered size="lg">
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                    Select a template
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Image src={homeTemplateImg1} className="img-fluid"/>
+                    <p>
+                    Option 1: Light minimalist design with blue and white accents.
+                    </p>
+                </Modal.Body>
+                
+                <Modal.Footer>
+                    <Button onClick={() => {dispatch(createWebsite());
+                    handleCloseTwo()}}>
+                        Select
+                    </Button>
+                    <Button variant="secondary"
+                        onClick={() => {
+                        handleCloseTwo();
+                        handleShowThree()}}
+                    >
+                        <BsChevronRight />
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            {'Selecting Template 2 Modal'}
+            <Modal show={show3} onHide={handleCloseThree}  centered size="lg">
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                    Select a template
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Image src={homeTemplateImg2} className="img-fluid"/>
+                    <p>
+                    Option 2: Dark minimalist design with blue and black accents.
+                    </p>
+                </Modal.Body>
+                
+                <Modal.Footer>
+                    <Button onClick={() => {dispatch(createWebsite());
+                    handleCloseThree()}}>
+                        Select
+                    </Button>
+                    <Button variant="secondary"
+                        onClick={() => {
+                        handleShowTwo();
+                        handleCloseThree()}}
+                    >
+                        <BsChevronLeft />
+                    </Button>
+                </Modal.Footer>
+            </Modal>
             
         </Container>
     )
