7d662e09664fbc59bae36fda2bff037db6828ab3
pgun0011 <69449953+pgun0011@users.noreply.github.com>
2021-05-25 15:27:33 +1000

    Award javascript file added

---
+++ client/src/components/awards/Award.js
@@ -0,0 +1,14 @@
+import './Award.css'
+
+
+const Award = () => {
+    return (
+        <main>                
+            <Container fluid>
+                You are currently viewing Award page. 
+            </Container>
+        </main>
+    )
+}
+
+export default Award
