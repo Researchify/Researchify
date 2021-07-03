61ede80d2c347ceb1f2b4b92eefb77349a60718f
riordanalfredo <riordan.alfredo@gmail.com>
2021-07-03 22:24:37 +1000

    remove the hyperlink and standardise the code with prettier

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,55 +1,63 @@
 /**
  * Root component.
  */
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Auth from './auth/Auth';
-import Home from './home/Home';
-import Dashboard from './dashboard/Dashboard';
-import ProfileInfoEdit from './profileInfoEdit/ProfileInfoEdit';
+import Auth from "./auth/Auth";
+import Home from "./home/Home";
+import Dashboard from "./dashboard/Dashboard";
+import ProfileInfoEdit from "./profileInfoEdit/ProfileInfoEdit";
 
-import Register from './auth/Register';
-import Login from './auth/Login';
-import Header from './layout/Header';
-import Sidebar from './layout/Sidebar';
-import { Container, Col, Row } from 'react-bootstrap';
+import Register from "./auth/Register";
+import Login from "./auth/Login";
+import Header from "./layout/Header";
+import Sidebar from "./layout/Sidebar";
+import { Container, Col, Row } from "react-bootstrap";
 
-import './layout/Layout.css'
+import "./layout/Layout.css";
 
-import PublicationPage from './publications/PublicationPage';
-import { Fragment } from 'react';
+import PublicationPage from "./publications/PublicationPage";
+import { Fragment } from "react";
 
 const App = () => {
-    const urls={
-        dashboard: "/dashboard",
-        profile: "/dashboard/profile"
-    }
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/auth" exact component={Auth} />
-                <Route path="/register" exact component={Register} />
-                <Route path="/login" exact component={Login} />
-                <Fragment>
-                    <Header title={"Researchify"} urls={urls}/>      
-                    <Container fluid>
-                        <Row>
-                        <Col className="sidebar-wrapper" md={2} lg={2} xl={1}>
-                            <Sidebar />
-                        </Col>
-                        <Col className="page-content-wrapper" md={10} lg={10} xl={11}>
-                            <Route path="/publications/team" exact component={PublicationPage}/>
-                            <Route path={urls.dashboard} exact component={Dashboard} />
-                            <Route path="/dashboard/profile" exact component={ProfileInfoEdit} />
-                        </Col>
-                        </Row>
-                    </Container>
-                </Fragment>                
-            </Switch>             
-        </BrowserRouter>
-    );
+  const urls = {
+    dashboard: "/dashboard",
+    profile: "/dashboard/profile",
+  };
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/auth" exact component={Auth} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/login" exact component={Login} />
+        <Fragment>
+          <Header title={"Researchify"} urls={urls} />
+          <Container fluid>
+            <Row>
+              <Col className="sidebar-wrapper" md={1} lg={1}>
+                <Sidebar />
+              </Col>
+              <Col className="page-content-wrapper" md={10} lg={10}>
+                <Route
+                  path="/publications/team"
+                  exact
+                  component={PublicationPage}
+                />
+                <Route path={urls.dashboard} exact component={Dashboard} />
+                <Route
+                  path="/dashboard/profile"
+                  exact
+                  component={ProfileInfoEdit}
+                />
+              </Col>
+            </Row>
+          </Container>
+        </Fragment>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
