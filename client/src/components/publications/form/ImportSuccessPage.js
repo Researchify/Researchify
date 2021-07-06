cb9b867591565a8fa88ac5f01a34da49c1e049dd
vgoh1 <vgoh0001@student.monash.edu>
2021-07-06 22:20:48 +1000

    Remove logging statements

--- client/src/components/publications/form/ImportSuccessPage.js
+++ client/src/components/publications/form/ImportSuccessPage.js
@@ -69,9 +69,7 @@ const ImportSuccessPage = ({closeModal}) => {
     }
 
     const handlePagination = () => {
-        console.log("handle pagination")
         dispatch(retrieveMorePublications(gScholarId, startFrom, teamId))
-        console.log(activePage);
     }
 
     const handleConfirmImport = () => {
