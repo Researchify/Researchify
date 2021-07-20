c36c5fed5c12fdeb8e779147493d54c61c581b81
vgoh1 <vgoh0001@student.monash.edu>
2021-07-20 20:55:23 +1000

    Removed unused types, tweak error reducer based off backend error return

--- client/src/error/errorReduxFunctions.js
+++ client/src/error/errorReduxFunctions.js
@@ -11,25 +11,14 @@ export const errorReducer = (state, action) => {
       error: null,
     };
   }
-  // TODO: update with better error handling message from API/server
-  /** something like this:
-   * {
-      "errorCode": "1001",
-      "errorMessage": "Validation error has occurred.",
-      "errors": {
-        "description": "Must not be null",
-        "name": "Must not be null"
-      },
-      "errorTimestamp": "2020-03-06T08:14:03.690Z"
-    }
-   */
   const res = action.payload.response;
+  console.log(res);
   return {
     ...state,
     error: {
-      data: res.data !== '' ? res.data : DEFAULT_ERROR_MESSAGE,
-      status: res.status,
-      statusText: res.statusText,
+      data: res.data !== '' ? res.data.errors[0] : DEFAULT_ERROR_MESSAGE,
+      status: res.data.errorCode,
+      statusText: res.data.errors[0],
     },
   };
 };
