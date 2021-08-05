7cf5a2e47ad772f1be6d459e09bcd4d2e74027a7
mable-yip <47068733+mable-yip@users.noreply.github.com>
2021-08-05 15:20:11 +1000

    Update toaster component

--- client/src/notification/NotificationToaster.js
+++ client/src/notification/NotificationToaster.js
@@ -5,21 +5,23 @@
 
 import React, { Fragment, useEffect } from 'react';
 import toast from 'react-hot-toast';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { CLEAR_NOTIFICATION } from '../actions/types';
 export const NotificationToaster = () => {
   const { error, success } = useSelector((state) => state.notification);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if(error){
       toast.error(error.data);
     }
-  }, [error])
-
-  useEffect(() => {
     if(success){
       toast.success(success);
     }
-  }, [success])
+    dispatch({
+      type: CLEAR_NOTIFICATION
+    })
+  }, [error, success])
   
   return <Fragment />;
 };
