5dd4c8f26bb977db7cdda2ac5a82f20b59f625c4
Jie Fan <jfan0010@student.monash.edu>
2021-08-06 12:56:45 +1000

    Apply collapse toggle in client header for mobile view

--- scholly/base/src/components/layout/Header.js
+++ scholly/base/src/components/layout/Header.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
   // dummy data passed in
@@ -9,14 +9,25 @@ const Header = () => {
 
   return (
     <Fragment>
-      <Navbar bg="light" variant="light" fixed="top">
-        <Navbar.Brand>{props.title}</Navbar.Brand>
-        <Nav className="mr-auto" />
-        <Nav>
-          <Nav.Link>Home</Nav.Link>
-          <Nav.Link>Team</Nav.Link>
-          <Nav.Link>Publications</Nav.Link>
-        </Nav>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="light"
+        variant="light"
+        fixed="top"
+      >
+        <Container fluid>
+          <Navbar.Brand>{props.title}</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto" />
+            <Nav>
+              <Nav.Link>Home</Nav.Link>
+              <Nav.Link>Team</Nav.Link>
+              <Nav.Link>Publications</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </Fragment>
   );
