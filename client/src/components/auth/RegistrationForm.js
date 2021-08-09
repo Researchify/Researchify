42e6ff0084dfa9a62653a705fce7b66b7ad5931e
mable-yip <47068733+mable-yip@users.noreply.github.com>
2021-08-09 20:23:06 +1000

    Update registrationForm to validate each input is acceptable before sending to backend

--- client/src/components/auth/RegistrationForm.js
+++ client/src/components/auth/RegistrationForm.js
@@ -2,7 +2,7 @@
  * This file exports a Registration Form component used to display registration input.
  */
 
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -11,139 +11,157 @@ import { createTeam } from '../../actions/team';
 import './Register.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import toast from 'react-hot-toast';
+import { Formik } from 'formik';
+import * as yup from 'yup';
 
 export default function RegistrationForm() {
   const dispatch = useDispatch();
   const history = useHistory();
-  const [validated, setValidated] = useState(false);
   const isRegistered = useSelector(state => state.auth.isRegistered)
 
   useEffect(() => {
-    if(isRegistered && validated){
+    if(isRegistered){
       history.push('/login')
     }
-  }, [history, isRegistered, validated])
+  }, [history, isRegistered])
 
-  const [inputs, setInputs] = useState({
+  const teamInfoSchema = yup.object({
+    teamName: yup
+      .string()
+      .required('Team Name is required')
+      .min(3, 'Team Name must be at least 3 characters'),
+    orgName: yup
+      .string()
+      .min(3, 'Organization Name must be at least 3 characters'),
+    email: yup
+      .string()
+      .email('Invalid Email')
+      .required('Email is required'),
+    password: yup
+      .string()
+      .required('Please Enter your password')
+      .matches(
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+        "Use 8 or more characters with a mix of letters, numbers & symbols"
+      ),
+    confirmedPassword: yup
+      .string()
+      .required('Please re-enter your password')
+      .oneOf([yup.ref('password'), null], 'Passwords must match'),
+  });
+
+  const initialTeamInfo = {
     teamName: '',
     orgName: '',
     email: '',
     password: '',
-    confirmPassword: '',
-  });
-
-  const updateValue = (form) => {
-    const { name, value } = form.target;
-    setInputs({ ...inputs, [name]: value });
+    confirmedPassword: '',
   };
 
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    event.preventDefault();
-    if (form.checkValidity() === false) {
-      event.stopPropagation();
-    } else{
-      if (inputs.password !== inputs.confirmPassword){
-        return toast.error('Passwords do not match');
-      }
-      const teamData = {
-        email: inputs.email,
-        teamName: inputs.teamName,
-        orgName: inputs.orgName,
-        password: inputs.password,
-        repoCreated: false,
-      };
-      dispatch(createTeam(teamData))
-    }
-    setValidated(true);
+  const submitForm = (values) => {
+    delete values.confirmedPassword
+    dispatch(createTeam(values))
   };
 
   return (
     <Jumbotron id="form-box">
       <h3 id="signUpHeading">Sign Up</h3>
       <hr />
-      <Form noValidate validated={validated} onSubmit={handleSubmit}>
-        <Form.Row>
-          <Form.Group as={Col} md="6">
-            <Form.Label>Team name</Form.Label>
-            <Form.Control
-              id="tName"
-              type="text"
-              onChange={updateValue}
-              name="teamName"
-              placeholder="Team name"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Please input a valid team name.
-            </Form.Control.Feedback>
+      <Formik
+        enableReinitialize
+        validationSchema={teamInfoSchema}
+        onSubmit={submitForm}
+        initialValues={initialTeamInfo}
+      >
+      {({ handleSubmit, handleChange, values, touched, errors }) => (
+        <Form noValidate onSubmit={handleSubmit} >
+          <Form.Row>
+            <Form.Group as={Col} md="6">
+              <Form.Label> Team name </Form.Label>
+                <Form.Control
+                  type="text"
+                  name="teamName"
+                  placeholder="Team name"
+                  value={values.teamName}
+                  onChange={handleChange}
+                  isInvalid={touched.teamName && errors.teamName}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.teamName}
+                </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group as={Col} md="6">
+              <Form.Label> Organization name </Form.Label>
+                <Form.Control
+                  type="text"
+                  name="orgName"
+                  placeholder="Organization name"
+                  value={values.orgName}
+                  onChange={handleChange}
+                  isInvalid={touched.orgName && errors.orgName}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.orgName}
+                </Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Group>
+            <Form.Label> Email address </Form.Label>
+              <Form.Control
+                type="text"
+                name="email"
+                placeholder="Email"
+                value={values.email}
+                onChange={handleChange}
+                isInvalid={touched.email && errors.email}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.email}
+              </Form.Control.Feedback>
           </Form.Group>
-          <Form.Group as={Col} md="6">
-            <Form.Label>Organization name</Form.Label>
-            <Form.Control
-              type="text"
-              onChange={updateValue}
-              name="orgName"
-              placeholder="Organization name"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Please input a valid Organization name.
-            </Form.Control.Feedback>
+
+          <Form.Group>
+            <Form.Label> Password </Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={values.password}
+                onChange={handleChange}
+                isInvalid={touched.password && errors.password}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.password}
+              </Form.Control.Feedback>
           </Form.Group>
-        </Form.Row>
 
-        <Form.Group controlId="formEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            onChange={updateValue}
-            name="email"
-            placeholder="Enter email"
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Please input a valid email address.
-          </Form.Control.Feedback>
-        </Form.Group>
+          <Form.Group>
+            <Form.Label> Confirm Password </Form.Label>
+              <Form.Control
+                type="password"
+                name="confirmedPassword"
+                placeholder="Password"
+                value={values.confirmedPassword}
+                onChange={handleChange}
+                isInvalid={touched.confirmedPassword && errors.confirmedPassword}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.confirmedPassword}
+              </Form.Control.Feedback>
+          </Form.Group>
 
-        <Form.Group controlId="formPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            onChange={updateValue}
-            name="password"
-            placeholder="Password"
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Please input a valid password.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group controlId="formConfirmedPassword">
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control
-            type="password"
-            onChange={updateValue}
-            name="confirmPassword"
-            placeholder="Password"
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Please input a valid confirmed password.
-          </Form.Control.Feedback>
-        </Form.Group>
-
-        <div>
-          <a id="signInLink" href="login">
-            Already have an account? Sign in
-          </a>
-          <Button id="submitButton" type="submit" variant="primary">
-            Sign Up
-          </Button>
+          <div>
+            <a id="signInLink" href="login">
+              Already have an account? Sign in
+            </a>
+            <Button id="submitButton" type="submit" variant="primary">
+              Sign Up
+            </Button>
         </div>
-      </Form>
+      </Form>)}
+    </Formik>
     </Jumbotron>
   );
 }
