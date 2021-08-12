2f316e4e240774e883005392677aaf4214c20ed2
Jie Fan <jfan0010@student.monash.edu>
2021-08-12 15:11:19 +1000

    Team Cheakpoint

--- scholly/base/src/components/team/TeamPage.js
+++ scholly/base/src/components/team/TeamPage.js
@@ -1,25 +1,27 @@
 /**
  * The TeamPage component displays a list of team members.
  */
-import {TEAM_MEMBERS} from '../../global/data';
+import React, { Fragment } from 'react';
+import { CardDeck, Container } from 'react-bootstrap';
+import { TEAM_MEMBERS } from '../../global/data';
+import TeamMember from './TeamMember';
 
 const TeamPage = () => {
-    const teamMembers = TEAM_MEMBERS;
-    return(
-        <>
-            <h2> Team Page </h2>
-            {
-                teamMembers.map((member, idx) => 
-                    <div key={idx}>
-                        <h4> Member {idx+1} </h4>
-                        <li> full Name: {member.fullName} </li>
-                        <li> position: {member.position} </li>
-                        <li> summary: {member.position} </li>
-                    </div>
-                )
-            }
-        </>
-    )
-}
+  const teamMembers = TEAM_MEMBERS;
+  return (
+    <Fragment>
+      <Container className="pages-top-padding text-center mt-3 mb-3">
+        <div className="team-pg-title">Meet Our Team</div>
+      </Container>
+      <Container fluid className="team-card-container">
+        <CardDeck>
+          {teamMembers.map((member) => (
+            <TeamMember member={member} key={member._id} />
+          ))}
+        </CardDeck>
+      </Container>
+    </Fragment>
+  );
+};
 
-export default TeamPage
+export default TeamPage;
