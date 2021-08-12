cdd8528c2ada35c5e256dc0bd229c050c6a34dc5
Sharan Sharabinth <ssha0054@student.monash.edu>
2021-08-12 15:10:11 +1000

    implement post request to create new achievement

---
+++ api/src/controllers/achievements.js
@@ -0,0 +1,36 @@
+/**
+ * @file This module contains handlers for the "achievements" route.
+ * @module achievements
+ */
+
+ const Achievement = require('../models/achievement.model');
+
+ const Team = require('../models/team.model');
+
+/**
+ * Handles a POST request, which will create an achievement/award in the database using the endpoint /achievements.
+ * @param req request object - achievement in body (see Achievement model)
+ * @param res response object
+ * @returns 201: the achievement has been created
+ * @returns 404: no team was found in the database to associate the achievement with
+ * @returns 500: server encountered an unexpected condition
+ */
+ function createAchievement(req, res, next) {
+    const achievement = req.body;
+    
+    // Check if teamId provided is part of an existing team
+    const result = Team.findById({ _id: achievement.teamId }).catch((err) =>
+      next(fillErrorObject(500, 'Server error', [err.errors]))
+    );
+
+    // Create achievement if teamId is valid, return error if not
+    if (result == null) {
+      next(fillErrorObject(404, 'Validation error', ['Team was not found']));
+    } else {
+        Achievement.create(achievement)
+        .then((createdAchievement) => res.status(201).json(createdAchievement))
+        .catch((err) => next(fillErrorObject(500, 'Server error', [err.errors])));
+    }
+  }
+
+  module.exports = { createAchievement }
---
+++ api/src/routes/achievements.js
@@ -0,0 +1,15 @@
+/**
+ * This module defines the endpoints for the "/achievements" route and exports the corresponding Router.
+ */
+
+ const achievementsRouter = require('express').Router();
+
+ const achievementsController = require('../controllers/achievements');
+ 
+ achievementsRouter.post('/', achievementsController.createAchievement);
+//  // achievementsRouter.get('/', achievementsController.getAchievements);
+//  // achievementsRouter.delete('/', achievementsController.removeAchievement);
+//  // achievementsRouter.patch('/', achievementsController.updateAchievement);
+ 
+ module.exports = achievementsRouter;
+ 
