f70ad43d23ab85a3feb2ed5b39b1d845f1cbaa51
vgoh1 <vgoh0001@student.monash.edu>
2021-08-12 14:53:43 +1000

    Add UI validation

--- client/src/components/twitter/TwitterLink.js
+++ client/src/components/twitter/TwitterLink.js
@@ -16,61 +16,65 @@ import Col from 'react-bootstrap/Col';
 const TwitterLink = () => {
   const dispatch = useDispatch();
   const [handle, setHandle] = useState('');
-  const [revealInput, setRevealInput] = useState(false);
   const teamId = useSelector((state) => state.team.teamId);
   const twitterFetchLoading = useSelector(
     (state) => state.team.twitterFetchLoading
   );
+  const [validated, setValidated] = useState(false);
 
-  const handleLinkButtonClick = () => {
-    setRevealInput(true);
-    dispatch(linkTwitter(teamId, handle));
+  const handleLinkButtonClick = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      console.log('in');
+    } else {
+      dispatch(linkTwitter(teamId, handle));
+    }
+    console.log(validated);
+
+    // setValidated(true);
   };
 
   const handleCancelButtonClick = () => {
     setHandle('');
-    setRevealInput(false);
   };
 
-  return twitterFetchLoading ? (
-    <div className="mb-3 mt-3 text-center">
-      <Spinner animation="border" />
-    </div>
-  ) : (
+  // twitterFetchLoading ? (
+  //   <div className="mb-3 mt-3 text-center">
+  //     <Spinner animation="border" />
+  //   </div>
+  // ) :
+  return (
     <Jumbotron className="twitter-link">
       <h6 className="twitter-link_link_message">Link your Twitter account?</h6>
-      {revealInput && (
-        <Form>
-          <Form.Row>
-            <Col>
-              <Form.Control
-                className="twitter-link_input"
-                size="sm"
-                placeholder="Enter handle"
-                value={handle}
-                onChange={(e) => setHandle(e.target.value)}
-              />
-            </Col>
-          </Form.Row>
-        </Form>
-      )}
-      <Button
-        type="submit"
-        size="sm"
-        className="twitter-link_button"
-        onClick={handleLinkButtonClick}
-      >
-        Link Twitter
-      </Button>
-      {revealInput && (
+      <Form noValidate validated={validated}>
+        <Form.Row>
+          <Col>
+            <Form.Control
+              required
+              minlength="1"
+              maxlength="15"
+              className="twitter-link_input"
+              size="sm"
+              placeholder="Enter handle"
+              value={handle}
+              onChange={(e) => setHandle(e.target.value)}
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide a Twitter handle.
+            </Form.Control.Feedback>
+          </Col>
+        </Form.Row>
         <Button
+          type="submit"
           size="sm"
-          variant="outline-secondary"
-          onClick={handleCancelButtonClick}
+          className="twitter-link_button"
+          onClick={handleLinkButtonClick}
         >
-          Cancel
+          Link Twitter
         </Button>
-      )}
+      </Form>
     </Jumbotron>
   );
 };
