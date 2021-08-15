041a724984f1f98f06486567312ced68b2917dda
mable-yip <47068733+mable-yip@users.noreply.github.com>
2021-08-15 20:02:27 +1000

    Add config file for storing page size varaible

--- scholly/base/src/components/publications/Publications.js
+++ scholly/base/src/components/publications/Publications.js
@@ -6,9 +6,10 @@ import { Accordion } from 'react-bootstrap';
 import Publication from './publication/Publication';
 import { TEAM_PUBLICATIONS } from '../../global/data';
 import usePagination from '../shared/usePagination';
+import { pageSize } from '../../config/publications';
 
 const Publications = () => {
-  const { currentData, pagination } = usePagination(TEAM_PUBLICATIONS, 10)
+  const { currentData, pagination } = usePagination(TEAM_PUBLICATIONS, pageSize)
 
   return (
     <Fragment>
