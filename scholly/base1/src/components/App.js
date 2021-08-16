22d3d7d191a7a99246c2a03601baf85884c7ad5d
Mel <mkur0011@student.monash.edu>
2021-08-16 18:56:58 +0400

    Basic Left Side Bar Implemented. Page contents accordingly aligned.

--- scholly/base1/src/components/App.js
+++ scholly/base1/src/components/App.js
@@ -5,20 +5,35 @@ import React, { Fragment } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import PublicationPage from './publications/PublicationPage.js';
 import LandingPage from './landingPage/LandingPage.js';
-import Header from './layout/Header.js';
 import TeamPage from './team/TeamPage.js';
+import {Container, Row, Col } from "react-bootstrap";
 import './centred.css';
+import Sidebar from "./layout/Sidebar";
+import "./layout/Sidebar.css"
 
 const App = () => {
   return (
     <Fragment>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route exact path="/publication" component={PublicationPage} />
-        <Route exact path="/team" component={TeamPage} />
-      </Switch>
+        <Container fluid>
+            <Row>
+                <Col xs={3} id="sidebar-wrapper"  md={3} lg={3} xl={2}>
+                    <Sidebar />
+
+                </Col>
+                <Col className="page-content-wrapper" md={8} lg={4} xl={9}>
+                    <Switch>
+                        <Route exact path="/" component={LandingPage} />
+                        <Route exact path="/publication" component={PublicationPage} />
+                        <Route exact path="/team" component={TeamPage} />
+                    </Switch>
+                </Col>
+            </Row>
+
+
+        </Container>
+
     </Fragment>
+
   );
 };
 
