216b27d25056c781d7c936dfcc70659638547c8c
Jie Fan <jfan0010@student.monash.edu>
2021-08-19 23:51:37 +1000

    homepage model

---
+++ api/src/models/homepage.model.js
@@ -0,0 +1,24 @@
+/**
+ * This module exports a "Homepage" mongoose Schema, which represents their homepage.
+ */
+const mongoose = require('mongoose');
+
+const homepageSchema = new mongoose.Schema(
+  {
+    teamId: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'team',
+      required: true,
+    },
+    aboutUs: [
+      {
+        type: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Homepage = mongoose.model('homepage', homepageSchema);
+
+module.exports = Homepage;
