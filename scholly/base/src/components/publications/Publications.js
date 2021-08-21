cb85eadfb7c5cc8eca28d66237903214810ef880
mable-yip <47068733+mable-yip@users.noreply.github.com>
2021-08-21 19:20:56 +1000

    Fix bug in schollu

--- scholly/base/src/components/publications/Publications.js
+++ scholly/base/src/components/publications/Publications.js
@@ -1,26 +1,57 @@
 /**
  * The Publications component displays a list of publications.
  */
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { Accordion } from 'react-bootstrap';
 import LayoutAllPublications from './publicationsLayout/LayoutAllPublications';
 import LayoutByCategory from './publicationsLayout/LayoutByCategory'
 import { TEAM_PUBLICATIONS } from '../../global/data';
 import PublicationsDropdown from './publicationsLayout/PublicationsDropdown';
 import { WEB_PAGES } from '../../global/data';
-import { layoutOption } from '../../config/publications';
+import { layoutOption, sortingOption } from '../../config/publications';
 
 const Publications = () => {
   const { publicationOptions } = WEB_PAGES
-  console.log(WEB_PAGES)
-  console.log(publicationOptions)
-  const [preference, setPreference] = useState(publicationOptions);
+  const [ options, setOptions ] = useState(publicationOptions);
 
+  const sortPublications = (teamPublications, option) => {
+    switch (option) {
+        case sortingOption.AUTHOR:
+            teamPublications.sort((a, b) =>
+                a.authors[0].toLowerCase() > b.authors[0].toLowerCase() ? 1 : -1
+            );
+            break;
+        case sortingOption.TITLE:
+            // publication title
+            teamPublications.sort((a, b) =>
+                a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1
+            );
+            break;
+        case 'Category Title':
+            // journal or conference title
+            teamPublications.sort((a, b) =>
+                a.category.categoryTitle.toLowerCase() >
+                b.category.categoryTitle.toLowerCase()
+                ? 1
+                : -1
+            );
+            break;
+        default:
+            // sort by title then year for consistency with the db
+            teamPublications.sort((a, b) =>
+                a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1
+            );
+            teamPublications.sort((a, b) => (a.year > b.year ? -1 : 1));
+            break;
+        }
+        return teamPublications
+    };
+    
+  const [ publications, setPublications ] = useState(sortPublications(TEAM_PUBLICATIONS, options.sortBy));
 
-  
   const renderPublications = () => {
-    console.log(preference)
-    switch (preference.layout) {
+    console.log(options)
+    switch (options.layout) {
       case layoutOption.BY_CATEGORY:
         return <LayoutByCategory teamPublications={TEAM_PUBLICATIONS}/>;
       default:
@@ -28,12 +59,14 @@ const Publications = () => {
     }
   };
 
+
   return (
     <Fragment>
       <PublicationsDropdown 
-        preference={preference}
-        setPreference={setPreference}
-        publication={TEAM_PUBLICATIONS}
+        options={options}
+        setOptions={setOptions}
+        publications={publications}
+        sortPublications={sortPublications}
       />
       <Accordion>
         {renderPublications()}
--- scholly/base/src/components/publications/publicationsLayout/PublicationsDropdown.js
+++ scholly/base/src/components/publications/publicationsLayout/PublicationsDropdown.js
@@ -3,54 +3,23 @@ import { Dropdown } from 'react-bootstrap';
 import { layoutOption, sortingOption } from '../../../config/publications';
 
 const PublicationsDropdown = ({
-    preference,
-    setPreference,
-    publication,
+    options,
+    setOptions,
+    publications,
+    sortPublications,
 }) => {
-    const sortPublications = (teamPublications, option) => {
-        switch (option) {
-            case sortingOption.AUTHOR:
-                teamPublications.sort((a, b) =>
-                    a.authors[0].toLowerCase() > b.authors[0].toLowerCase() ? 1 : -1
-                );
-                break;
-            case sortingOption.TITLE:
-                // publication title
-                teamPublications.sort((a, b) =>
-                    a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1
-                );
-                break;
-            case 'Category Title':
-                // journal or conference title
-                teamPublications.sort((a, b) =>
-                    a.category.categoryTitle.toLowerCase() >
-                    b.category.categoryTitle.toLowerCase()
-                    ? 1
-                    : -1
-                );
-                break;
-            default:
-                // sort by title then year for consistency with the db
-                teamPublications.sort((a, b) =>
-                    a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1
-                );
-                teamPublications.sort((a, b) => (a.year > b.year ? -1 : 1));
-                break;
-            }
-        };
-
     return(
         <div style={{ display: 'flex', justifyContent: 'center' }}>
             <Dropdown style={{marginRight: '3px'}}>
                 <Dropdown.Toggle variant="light" className="mb-2">
-                Layout: {preference.layout}
+                Layout: {options.layout}
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
                 {Object.keys(layoutOption).map((layout, i) => (
                     <Dropdown.Item
                         key={i}
                         as="button"
-                        onClick={() => setPreference({...preference, layout: layoutOption[layout]})}
+                        onClick={() => setOptions({...options, layout: layoutOption[layout]})}
                     >
                     {layoutOption[layout]}
                     </Dropdown.Item>
@@ -60,7 +29,7 @@ const PublicationsDropdown = ({
 
             <Dropdown style={{marginLeft: '3px'}}>
                 <Dropdown.Toggle variant="light" className="mb-2">
-                    Sort by: {preference.sortBy}
+                    Sort by: {options.sortBy}
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
                 {Object.keys(sortingOption).map((sortBy, i) => (
@@ -69,20 +38,20 @@ const PublicationsDropdown = ({
                     as="button"
                     value={sortingOption[sortBy]}
                     onClick={(e) => {
-                        setPreference({...preference, sortBy: sortingOption[sortBy]})
-                        sortPublications(publication, e.target.value);
+                        setOptions({...options, sortBy: sortingOption[sortBy]})
+                        sortPublications(publications, e.target.value);
                     }}
                     >
                     {sortingOption[sortBy]}
                     </Dropdown.Item>
                 ))}
-                {preference.layout === layoutOption.BY_CATEGORY && 
+                {options.layout === layoutOption.BY_CATEGORY && 
                     <Dropdown.Item
                         as="button"
                         value="Category Title"
                         onClick={(e) => {
-                            setPreference({...preference, sortBy: e.target.value})
-                            sortPublications(publication, e.target.value);
+                            setOptions({...options, sortBy: e.target.value})
+                            sortPublications(publications, e.target.value);
                         }}
                     >
                         Category Title
