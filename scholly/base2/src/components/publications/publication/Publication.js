c5ffd5ec8bd432be2c863e31a4818775fe4f9a81
Sanath Mahesh <sana0007@student.monash.edu>
2021-08-21 20:58:26 +0400

    Publication page data in modal done

--- scholly/base2/src/components/publications/publication/Publication.js
+++ scholly/base2/src/components/publications/publication/Publication.js
@@ -1,13 +1,10 @@
 /**
  * The Publication component displays a single publication.
  */
-import React, { useEffect, useState } from 'react';
+import React, {  useState } from 'react';
 import { Accordion, Card, Modal, Button } from 'react-bootstrap';
 
 const Publication = ({ pub }) => {
-  const [show, setShow] = useState(false);
-
-
   const [modalShow, setModalShow] = React.useState(false);
 
   return (
@@ -23,73 +20,67 @@ const Publication = ({ pub }) => {
         <Button variant="primary" className="button-pub" onClick={() => setModalShow(true)}>
           View this Publication
         </Button>
-        <MyVerticallyCenteredModal
+        <PublicationModal
           pub ={pub}
           show={modalShow}
           onHide={() => setModalShow(false)}
         />
-
       </Accordion.Toggle>
-      <Accordion.Collapse eventKey={pub._id}>
-        <Card.Body className="publication-body-column">
-          <div className="pub-body-subheader">Authors</div>
-          <div className="pub-body-content">
-            {pub.authors.map((author) => `${author}`).join(', ')}
-          </div>
-          <div className="pub-body-subheader">Description</div>
-          <div className="pub-body-content pub-body-paragraph">
-            {pub.description}
-          </div>
-          <div className="pub-body-subheader">
-            {pub.category.categoryTitle
-              ? pub.category.type.charAt(0) +
+    </Card>
+  );
+
+  function PublicationModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            {pub.title}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Card.Body className="publication-body-column">
+            <div className="pub-body-subheader">Authors</div>
+            <div className="pub-body-content">
+              {pub.authors.map((author) => `${author}`).join(', ')}
+            </div>
+            <div className="pub-body-subheader">Description</div>
+            <div className="pub-body-content pub-body-paragraph">
+              {pub.description}
+            </div>
+            <div className="pub-body-subheader">
+              {pub.category.categoryTitle
+                ? pub.category.type.charAt(0) +
                 pub.category.type.slice(1).toLowerCase()
-              : ''}
-          </div>
-          <div className="pub-body-content">
-            {pub.category.categoryTitle
-              ? pub.category.categoryTitle +
+                : ''}
+            </div>
+            <div className="pub-body-content">
+              {pub.category.categoryTitle
+                ? pub.category.categoryTitle +
                 (pub.category.issue ? ', Issue ' + pub.category.issue : '') +
                 (pub.category.volume ? ', Volume ' + pub.category.volume : '') +
                 (pub.category.pages ? ', Page ' + pub.category.pages : '')
-              : ''}
-          </div>
-          <div className="pub-body-subheader">
-            {pub.category.publisher ? 'Published by' : null}
-          </div>
-          <div className="pub-body-content">{pub.category.publisher}</div>
-        </Card.Body>
-      </Accordion.Collapse>
-    </Card>
-  );
+                : ''}
+            </div>
+            <div className="pub-body-subheader">
+              {pub.category.publisher ? 'Published by' : null}
+            </div>
+            <div className="pub-body-content">{pub.category.publisher}</div>
+          </Card.Body>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+
 };
 
-function MyVerticallyCenteredModal(props) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
 
 export default Publication;
