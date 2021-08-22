c2e42a25aa24d09082b21b94ed1c3f4d62a62bca
rsal0007 <rsal0007@student.monash.edu>
2021-08-22 11:50:22 +0530

    removed empty spaces between LOC

--- scholly/base3/src/components/team/TeamMember.js
+++ scholly/base3/src/components/team/TeamMember.js
@@ -17,7 +17,6 @@
          <Card className = "shadow text-center" bg = {teamMembers.indexOf(member) % 2 === 0 ? "white" : "secondary"}  > 
                        <Card.Body >
                        <Row>
-                       
                        <Card.Img style={{width: "160px", height: "120px"}}  src={profilePicture} className="team-member-picture " />
                        <Col>
                        <Card.Title as="h5">{member.fullName}</Card.Title>
@@ -29,8 +28,6 @@
            </Card>
            </Container>
        </Fragment>
- 
-
    )
  }
  
