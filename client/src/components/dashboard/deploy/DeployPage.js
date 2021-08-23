c1cb1959286e22b6acbd0bc60f7fbdfae9296435
Jie Fan <jfan0010@student.monash.edu>
2021-08-23 19:21:19 +1000

    removed comment, added toast

--- client/src/components/dashboard/deploy/DeployPage.js
+++ client/src/components/dashboard/deploy/DeployPage.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { GoMarkGithub } from 'react-icons/go';
 import { Button, Spinner } from 'react-bootstrap';
 import GitHubLogin from 'react-github-login';
+import toast from 'react-hot-toast';
 
 import { githubClientId, scope } from '../../../config/deploy';
 import { getGHAccessToken, deployToGHPages } from '../../../actions/team';
@@ -10,7 +11,6 @@ import { getGHAccessToken, deployToGHPages } from '../../../actions/team';
 const DeployPage = ({ teamId }) => {
   const dispatch = useDispatch();
   const loading = useSelector((state) => state.deploy.loading);
-  // TODO: refactor this into useState
   const retrievedAccessToken = useSelector(
     (state) => state.team.retrievedAccessToken
   );
@@ -28,25 +28,11 @@ const DeployPage = ({ teamId }) => {
     dispatch(getGHAccessToken(teamId, code));
   }
 
-  const onLoginFail = (response) => {
-    // TODO, show a nice error.
-    console.error(response);
+  // handle error toast when fail to log in
+  // usually is when user close the login window
+  const onLoginFail = () => {
+    toast.error("You must login with GitHub to deploy");
   }
-
-  // useEffect(() => {
-  //   // github returns a code in the url after user logs in
-  //   const url = window.location.href;
-  //   const hasCode = url.includes('?code=');
-  //   if (hasCode && !retrievedAccessToken && teamId) {
-  //     const code = url.split('?code=')[1];
-  //     // we use this code to exchange an access token
-  //     dispatch(getGHAccessToken(teamId, code));
-  //   } else if (!retrievedAccessToken) {
-  //     // we refreshed so we should clear local storage
-  //     localStorage.clear();
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [dispatch, teamId]);
 
   const GitHubLoginButton = (
     <GitHubLogin className="float-right" clientId={githubClientId} scope={scope}
@@ -60,7 +46,6 @@ const DeployPage = ({ teamId }) => {
     <Button
       className="float-right"
       variant="primary"
-      // TODO: modify this condition to check currentWebPages isn't empty
       disabled={!retrievedAccessToken}
       onClick={handleDeploy}
     >
@@ -69,7 +54,7 @@ const DeployPage = ({ teamId }) => {
   );
 
   return (
-    <>
+    <Fragment>
       Deploy Website with GitHub
       {
         loading ?
@@ -78,7 +63,7 @@ const DeployPage = ({ teamId }) => {
         </div> :
         ( retrievedAccessToken ? DeployButton : GitHubLoginButton )
       }
-    </>
+    </Fragment>
   );
 };
 
