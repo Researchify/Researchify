c8ba339d8d0db8af611d2bda50866cf8cf498905
Akash Saggar <asag0003@student.monash.edu>
2021-08-27 10:40:44 +1000

    Ensure theme inputs are Int before saving to DB

--- api/src/controllers/website.js
+++ api/src/controllers/website.js
@@ -129,8 +129,8 @@ function deleteWebPage(req, res, next) {
 function updateThemeOptions(req, res, next) {
   const { team_id } = req.params;
   const update = {
-    color: req.body.color,
-    layout: req.body.layout,
+    color: parseInt(req.body.color),
+    layout: parseInt(req.body.layout),
   };
   Website.findOneAndUpdate({ teamId: team_id }, update, {
     new: true,
