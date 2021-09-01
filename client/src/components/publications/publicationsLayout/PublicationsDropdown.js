11d10dc39e7d6d35a18b04fe6d3941d1d5b0a124
vgoh1 <40584352+vgoh1@users.noreply.github.com>
2021-09-01 20:06:49 +1000

    Remove prettier config from repo (#199)
    * Remove prettier config
    
    * Remove prettier config from client
    
    * See if GH throws error for formatting
    
    * Remove unused imports and prettier config from scholly base
    
    * Format landing page files
    
    * Remove contact us page
    
    * PFB

--- client/src/components/publications/publicationsLayout/PublicationsDropdown.js
+++ client/src/components/publications/publicationsLayout/PublicationsDropdown.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Dropdown, Button } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { sortingOptions, layoutOptions } from '../../../config/publications';
-import { updatePublicationOptions } from '../../../actions/website'
+import { updatePublicationOptions } from '../../../actions/website';
 
 const PublicationsDropdown = ({
   options,
@@ -14,7 +14,7 @@ const PublicationsDropdown = ({
   const dispatch = useDispatch();
 
   const handleUpdate = () => {
-    dispatch(updatePublicationOptions(teamId, options))
+    dispatch(updatePublicationOptions(teamId, options));
   };
 
   return (
@@ -22,14 +22,16 @@ const PublicationsDropdown = ({
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <Dropdown>
           <Dropdown.Toggle variant="light" className="mb-2">
-            Layout: {options.layout}
+            Layout:
+            {' '}
+            {options.layout}
           </Dropdown.Toggle>
           <Dropdown.Menu>
             {Object.keys(layoutOptions).map((layout, i) => (
               <Dropdown.Item
                 key={i}
                 as="button"
-                onClick={() => setOptions({...options, layout: layoutOptions[layout]})}
+                onClick={() => setOptions({ ...options, layout: layoutOptions[layout] })}
               >
                 {layoutOptions[layout]}
               </Dropdown.Item>
@@ -37,9 +39,11 @@ const PublicationsDropdown = ({
           </Dropdown.Menu>
         </Dropdown>
 
-        <Dropdown >
+        <Dropdown>
           <Dropdown.Toggle variant="light" className="mb-2">
-            Sort by: {options.sortBy}
+            Sort by:
+            {' '}
+            {options.sortBy}
           </Dropdown.Toggle>
           <Dropdown.Menu>
             {Object.keys(sortingOptions).map((sortBy, i) => (
@@ -48,33 +52,35 @@ const PublicationsDropdown = ({
                 as="button"
                 value={sortingOptions[sortBy]}
                 onClick={(e) => {
-                  setOptions({...options, sortBy: sortingOptions[sortBy]})
+                  setOptions({ ...options, sortBy: sortingOptions[sortBy] });
                   sortPublications(publication, e.target.value);
                 }}
               >
                 {sortingOptions[sortBy]}
               </Dropdown.Item>
             ))}
-            {options.layout === layoutOptions.BY_CATEGORY && 
+            {options.layout === layoutOptions.BY_CATEGORY
+              && (
               <Dropdown.Item
                 as="button"
                 value="Category Title"
                 onClick={(e) => {
-                  setOptions({...options, sortBy: e.target.value})
+                  setOptions({ ...options, sortBy: e.target.value });
                   sortPublications(publication, e.target.value);
-              }}
+                }}
               >
                 Category Title
               </Dropdown.Item>
-            }
+              )}
           </Dropdown.Menu>
         </Dropdown>
       </div>
-      <div className='mb-3' style={{ display: 'flex', justifyContent: 'center' }}>
-        <Button variant="secondary"
+      <div className="mb-3" style={{ display: 'flex', justifyContent: 'center' }}>
+        <Button
+          variant="secondary"
           onClick={handleUpdate}
-        > 
-          Update Layout & Sorting Options 
+        >
+          Update Layout &amp; Sorting Options
         </Button>
       </div>
     </div>
