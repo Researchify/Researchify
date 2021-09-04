4230f169eb37d6b49d5521c5ff42b86b32267730
mable-yip <47068733+mable-yip@users.noreply.github.com>
2021-09-05 00:07:44 +1000

    update fake data file

--- scholly/base/src/global/fakeData.js
+++ scholly/base/src/global/fakeData.js
@@ -1,6 +1,7 @@
 export const FAKE_WEB_PAGES = {
   pages: ['PUBLICATIONS', 'TEAM'],
   publicationOptions: { layout: 'All Publication', sortBy: 'Author' },
+  title: 'Fake title',
 };
 
 export const FAKE_PUBLICATIONS = [
