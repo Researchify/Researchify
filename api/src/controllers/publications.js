3489d7761cb0ee335195e213e04daa098440495a
vgoh1 <vgoh0001@student.monash.edu>
2021-09-05 13:56:54 +1000

    Try downloading one firefox only

--- api/src/controllers/publications.js
+++ api/src/controllers/publications.js
@@ -4,7 +4,7 @@
  */
 const mongoose = require('mongoose');
 const logger = require('winston');
-const { firefox } = require('playwright');
+const { firefox } = require('playwright-firefox');
 
 const { categoryTypes } = require('../config/publication');
 const { playwrightConfig } = require('../config/playwright');
