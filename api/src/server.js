1bfafe125ea3bc7456b810e2425a8b40949bb7d5
Sharan Sharabinth <ssha0054@student.monash.edu>
2021-09-11 18:24:16 +1000

    Fix indentations

--- api/src/server.js
+++ api/src/server.js
@@ -2,8 +2,8 @@
  * This module starts an express REST API server.
  */
 const app = require('./app');
+const PORT = process.env.PORT || 5000;
 const logger = require('./config/log');
 
 // Listen for connections
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => logger.info(`Server running on port: ${PORT}`));
