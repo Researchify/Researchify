5b4de417b3a5f21addf16d08b017261db44f7cde
Sharan Sharabinth <ssha0054@student.monash.edu>
2021-09-11 18:28:15 +1000

    Fix eslint

--- api/src/server.js
+++ api/src/server.js
@@ -2,8 +2,9 @@
  * This module starts an express REST API server.
  */
 const app = require('./app');
-const PORT = process.env.PORT || 5000;
 const logger = require('./config/log');
+
+const PORT = process.env.PORT || 5000;
 
 // Listen for connections
 app.listen(PORT, () => logger.info(`Server running on port: ${PORT}`));
