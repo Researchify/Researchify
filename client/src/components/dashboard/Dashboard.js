398ffb0f5791b8ae45dbe382fc8290dca982c030
Sanath Mahesh <sana0007@student.monash.edu>
2021-09-18 22:44:59 +0400

    Implemented the Modal

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,7 +4,7 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import {
-  Container, Card, Tabs, Tab,
+  Container, Card, Tabs, Tab, Modal, Button,
 } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import TemplateSelector from './TemplateSelector';
@@ -25,6 +25,8 @@ const Dashboard = () => {
 
   const [currentTab, setCurrentTab] = useState('home');
 
+  const [modalShow, setModalShow] = React.useState(true);
+
   const directToAnotherPage = (pageName) => {
     switch (pageName) {
       case 'PUBLICATIONS':
@@ -44,6 +46,10 @@ const Dashboard = () => {
   return (
     <main>
       <Container fluid className="p-5">
+        <WelcomeModal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+        />
         <Tabs
           defaultActiveKey="home"
           onSelect={(k) => setCurrentTab(k)}
@@ -77,6 +83,29 @@ const Dashboard = () => {
       </Container>
     </main>
   );
+  function WelcomeModal(props) {
+    return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+      <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter">
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Welcome
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Card.Body className="publication-body-column">
+            <div className="pub-body-subheader">
+              Welcome To Researchify!
+            </div>
+          </Card.Body>
+        </Modal.Body>
+        <Modal.Footer>
+          {/* eslint-disable-next-line react/destructuring-assignment,react/prop-types */}
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
 };
 
 export default Dashboard;
