6c806809194dac50793679096aadd381f837bead
mable-yip <47068733+mable-yip@users.noreply.github.com>
2021-09-18 22:03:40 +1000

    update login form from main

--- client/src/components/auth/authComponents/LoginForm.jsx
+++ client/src/components/auth/authComponents/LoginForm.jsx
@@ -1,79 +1,94 @@
 /**
  * This file exports an Auth component used to display sign-ins and sign-ups.
  */
-import React, { useState } from 'react';
+import React from 'react';
 import { Jumbotron, Form } from 'react-bootstrap';
 import '../css/login-form.css';
 import { useDispatch } from 'react-redux';
 import { Button } from '@material-ui/core';
+import { Formik } from 'formik';
+import * as yup from 'yup';
 import { login } from '../../../actions/auth';
 
 const LoginForm = () => {
   const dispatch = useDispatch();
-  const [inputs, setInputs] = useState({
-    email: '',
-    password: '',
+
+  const teamInfoSchema = yup.object({
+    email: yup
+      .string()
+      .required('Please Enter your Email'),
+    password: yup
+      .string()
+      .required('Please Enter your password'),
   });
 
-  const updateValue = (form) => {
-    const { name, value } = form.target;
-    setInputs({ ...inputs, [name]: value });
+  const authData = {
+    email: '',
+    password: '',
   };
 
-  const [validated, setValidated] = useState(false);
-
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    event.preventDefault();
-    if (form.checkValidity() === false) {
-      event.stopPropagation();
-    }
-    setValidated(true);
-    dispatch(login(inputs));
+  const submitForm = (values, { setFieldError }) => {
+    // error message could be passed in the setFieldError function to show error on the form
+    dispatch(login(values, setFieldError));
   };
 
   return (
-
     <Jumbotron id="login-form-box">
       <h3 id="LoginHeading">Log In</h3>
       <hr />
 
-      <Form noValidate validated={validated} onSubmit={handleSubmit}>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            onChange={updateValue}
-            name="email"
-            placeholder="Enter email"
-            required
-          />
-        </Form.Group>
-
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            onChange={updateValue}
-            name="password"
-            placeholder="Password"
-            required
-          />
-        </Form.Group>
-
-        <Button
-          id="loginButton"
-          type="submit"
-          variant="contained"
-          color="secondary"
-          size="large"
-          style={{ color: 'white' }}
-        >
-          Log in
-        </Button>
-      </Form>
+      <Formik
+        enableReinitialize
+        validationSchema={teamInfoSchema}
+        onSubmit={submitForm}
+        initialValues={authData}
+      >
+        {({
+          handleSubmit, handleChange, values, touched, errors,
+        }) => (
+          <Form noValidate onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Label> Email address </Form.Label>
+              <Form.Control
+                type="text"
+                name="email"
+                placeholder="Email"
+                value={values.email}
+                onChange={handleChange}
+                isInvalid={touched.email && errors.email}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.email}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label> Password </Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={values.password}
+                onChange={handleChange}
+                isInvalid={touched.password && errors.password}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.password}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Button
+              id="loginButton"
+              type="submit"
+              variant="contained"
+              color="secondary"
+              size="large"
+              style={{ color: 'white' }}
+            >
+              Log in
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </Jumbotron>
-
   );
 };
 
