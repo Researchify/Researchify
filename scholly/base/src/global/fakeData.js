01701bf0c2942b62a98fae1b8e5c66ebd6cd0cf5
Akash Saggar <asag0003@student.monash.edu>
2021-09-19 15:14:47 +1000

    Updated colours for layout 2

--- scholly/base/src/global/fakeData.js
+++ scholly/base/src/global/fakeData.js
@@ -182,7 +182,7 @@ export const FAKE_TEAM_HOMEPAGE = {
   aboutUs: ['This is first paragraph.', 'This is second paragraph.', 'This is third paragraph.'],
 };
 
-export const FAKE_LAYOUT_OPTION = 1;
+export const FAKE_LAYOUT_OPTION = 2;
 
 export const FAKE_TEAM_ACHIEVEMENTS = [
   {
