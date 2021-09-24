1c3246fdf94f95c52fce1935d9798a5b1a844662
Jie Fan <jfan0010@student.monash.edu>
2021-09-24 20:46:17 +1000

    move css to inline, since we dont like css file

--- client/src/components/twitter/TwitterFeed.js
+++ client/src/components/twitter/TwitterFeed.js
@@ -1,35 +1,52 @@
 /**
- * The TwitterFeed component displays a panel that renders a linked twitter account's feed.
+ * The TwitterFeed component displays a panel that renders a linked
+ * twitter account's feed.
  */
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Timeline } from 'react-twitter-widgets';
 import { unlinkTwitter } from '../../actions/team';
-import './TwitterFeed.css';
 import { PrimaryButton } from '../shared/styledComponents';
 
+// css styles
+const styles = {
+  twitterFeed: {
+    paddingTop: '15px',
+    paddingBottom: '30px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    borderWidth: '1px',
+    borderColor: '#ccc',
+    borderStyle: 'solid',
+    borderRadius: '16px',
+    position: 'fixed',
+  },
+};
+
+// function of changing twitter height according window
+const twitterTimelineHeight = () => {
+  const windowHeight = window.innerHeight;
+  switch (true) {
+    case (windowHeight >= 1000):
+      return { height: 800 };
+    case (windowHeight < 1000 && windowHeight >= 800):
+      return { height: 600 };
+    case (windowHeight < 800 && windowHeight >= 600):
+      return { height: 400 };
+    default:
+      return { height: 300 };
+  }
+};
+
+// function returning twitter component to be rendered
 const TwitterFeed = () => {
   // get team from state
   const dispatch = useDispatch();
   const team = useSelector((state) => state.team);
 
-  // adjust the height of twitter according to the size of window
-  const twitterTimelineHeight = () => {
-    const windowHeight = window.innerHeight;
-    switch (true) {
-      case (windowHeight >= 1000):
-        return { height: 800 };
-      case (windowHeight < 1000 && windowHeight >= 800):
-        return { height: 600 };
-      case (windowHeight < 800 && windowHeight >= 600):
-        return { height: 400 };
-      default:
-        return { height: 300 };
-    }
-  };
-
   return (
-    <div className="twitter-feed">
+    <div style={styles.twitterFeed}>
       <Timeline
         dataSource={{ sourceType: 'profile', screenName: team.twitterHandle }}
         options={twitterTimelineHeight()}
