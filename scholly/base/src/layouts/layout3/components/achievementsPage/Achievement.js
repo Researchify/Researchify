b0226cbb40d33c8a01f9f846bdf2599d0819b9b9
Andrew Cao <ancao@Andrews-MacBook-Pro.local>
2021-09-24 22:58:17 +1000

    Completely updated team page UI, refactored shared css

--- scholly/base/src/layouts/layout3/components/achievementsPage/Achievement.js
+++ scholly/base/src/layouts/layout3/components/achievementsPage/Achievement.js
@@ -11,14 +11,13 @@ const Achievement = ({ achievement }) => {
   bgTheme = (bgTheme === 'bg-secondary') ? 'bg-light' : 'bg-secondary';
 
   return (
-
     <div className="row mb-2" style={{ width: '70%' }}>
       <div className="row g-0">
         <div className="col-md-auto h-100">
           <Card.Img variant="top" src={awardPicture} style={{ height: '100px', width: '100px' }} />
         </div>
         <div className="col achievementBody">
-          <div className="award-name d-block w-100 text-left">{achievement.title}</div>
+          <div className="fontx award-name d-block w-100 text-left">{achievement.title}</div>
           <div className="award-year d-block w-100 text-left">{achievement.yearAwarded}</div>
           <div className="award-summary d-block w-100 text-left">{achievement.description}</div>
         </div>
