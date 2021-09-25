75767cd54fc1e95b0c659a8b984496e8181f75c6
Mel <mkur0011@student.monash.edu>
2021-09-25 22:48:23 +0400

    UI Fixes for Thumbnails

--- client/src/components/templateSelectorPage/TemplateSelector.js
+++ client/src/components/templateSelectorPage/TemplateSelector.js
@@ -4,7 +4,7 @@
  */
 import React, { useState, useEffect } from 'react';
 import {
-  Container, Form, Col,
+  Container, Form, Col, Card,
 } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { PropTypes } from 'prop-types';
@@ -56,85 +56,87 @@ const TemplateSelector = (props) => {
 
   return (
     <>
-      <b>Choose a Theme</b>
-      <Form.Check
-        type="switch"
-        id="custom-switch"
-        checked={darkMode}
-        onChange={() => setDarkMode(!darkMode)}
-        label="Use Dark mode"
-        className="ml-3 mt-2"
-      />
+      <Card style={{ padding: 50, border: 0 }}>
+        <b>Choose a Theme</b>
+        <Form.Check
+          type="switch"
+          id="custom-switch"
+          checked={darkMode}
+          onChange={() => setDarkMode(!darkMode)}
+          label="Use Dark mode"
+          className="ml-3 mt-2"
+        />
 
-      <Form
-        className="researchify-github-form mt-4"
-        noValidate
-        onSubmit={handleSubmit}
-      >
+        <Form
+          className="researchify-github-form mt-4"
+          noValidate
+          onSubmit={handleSubmit}
+        >
 
-        <Form.Group controlId="layout">
-          <Form.Label><b>Select a Layout</b></Form.Label>
-          <Container fluid>
-            <Form.Row>
-              <Col className="layout-display" xs={12} xl={4} sm={6}>
-                {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-                <label htmlFor="layout1">
-                  <Form.Check
-                    checked={layout === '1'}
-                    inline
-                    id="layout1"
-                    type="radio"
-                    label="Layout 1"
-                    name="layout"
-                    value={1}
-                    className="form-radio-text"
-                    onChange={updateSelections}
-                  />
-                  <LayoutThumbnail layoutOption={1} />
-                </label>
-              </Col>
-              <Col className="layout-display" xs={12} xl={4} sm={6}>
-                {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-                <label htmlFor="layout2">
-                  <Form.Check
-                    checked={layout === '2'}
-                    inline
-                    id="layout2"
-                    type="radio"
-                    label="Layout 2"
-                    name="layout"
-                    value={2}
-                    className="form-radio-text"
-                    onChange={updateSelections}
-                  />
-                  <LayoutThumbnail layoutOption={2} />
-                </label>
-              </Col>
-              <Col className="layout-display" xs={12} xl={4} sm={12}>
-                {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-                <label htmlFor="layout3">
-                  <Form.Check
-                    checked={layout === '3'}
-                    inline
-                    id="layout3"
-                    type="radio"
-                    label="Layout 3"
-                    name="layout"
-                    value={3}
-                    className="form-radio-text"
-                    onChange={updateSelections}
-                  />
-                  <LayoutThumbnail layoutOption={3} />
-                </label>
-              </Col>
-            </Form.Row>
-          </Container>
-        </Form.Group>
+          <Form.Group controlId="layout">
+            <Form.Label><b>Select a Layout</b></Form.Label>
+            <Container fluid>
+              <Form.Row>
+                <Col className="layout-display" xs={12} xl={4} sm={6}>
+                  {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+                  <label htmlFor="layout1">
+                    <Form.Check
+                      checked={layout === '1'}
+                      inline
+                      id="layout1"
+                      type="radio"
+                      label="Layout 1"
+                      name="layout"
+                      value={1}
+                      className="form-radio-text"
+                      onChange={updateSelections}
+                    />
+                    <LayoutThumbnail layoutOption={1} />
+                  </label>
+                </Col>
+                <Col className="layout-display" xs={12} xl={4} sm={6}>
+                  {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+                  <label htmlFor="layout2">
+                    <Form.Check
+                      checked={layout === '2'}
+                      inline
+                      id="layout2"
+                      type="radio"
+                      label="Layout 2"
+                      name="layout"
+                      value={2}
+                      className="form-radio-text"
+                      onChange={updateSelections}
+                    />
+                    <LayoutThumbnail layoutOption={2} />
+                  </label>
+                </Col>
+                <Col className="layout-display" xs={12} xl={4} sm={12}>
+                  {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+                  <label htmlFor="layout3">
+                    <Form.Check
+                      checked={layout === '3'}
+                      inline
+                      id="layout3"
+                      type="radio"
+                      label="Layout 3"
+                      name="layout"
+                      value={3}
+                      className="form-radio-text"
+                      onChange={updateSelections}
+                    />
+                    <LayoutThumbnail layoutOption={3} />
+                  </label>
+                </Col>
+              </Form.Row>
+            </Container>
+          </Form.Group>
 
-        <PrimaryButton id="submitButton" type="submit">
-          Update
-        </PrimaryButton>
-      </Form>
+          <PrimaryButton id="submitButton" type="submit">
+            Update
+          </PrimaryButton>
+        </Form>
+      </Card>
     </>
   );
 };
