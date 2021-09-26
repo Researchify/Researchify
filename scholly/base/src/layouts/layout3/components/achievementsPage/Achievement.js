02636a0b07e774c01b0566403d3de66b50821eac
Andrew Cao <ancao@Andrews-MacBook-Pro.local>
2021-09-26 21:21:38 +1000

    Updated image for achievements

--- scholly/base/src/layouts/layout3/components/achievementsPage/Achievement.js
+++ scholly/base/src/layouts/layout3/components/achievementsPage/Achievement.js
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import { Card } from 'react-bootstrap';
-import awardPicture from '../../../../shared/images/awardpic.jpg';
+import awardPicture from '../../../../shared/images/certificatepic.png';
 
 let bgTheme = 'bg-secondary';
 const Achievement = ({ achievement }) => {
@@ -12,9 +12,9 @@ const Achievement = ({ achievement }) => {
 
   return (
     <div className="row mb-2" style={{ width: '70%' }}>
-      <div className="row g-0">
+      <div className="row g-0" id="achievementImage">
         <div className="col-md-auto h-100">
-          <Card.Img variant="top" src={awardPicture} style={{ height: '100px', width: '100px' }} />
+          <Card.Img variant="top" src={awardPicture} style={{ height: '60px', width: '60px' }} />
         </div>
         <div className="col achievementBody">
           <div className="fontx award-name d-block w-100 text-left">{achievement.title}</div>
