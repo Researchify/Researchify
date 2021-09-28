7d4a812165fee38ceb35e49531fa65963815c436
mable-yip <47068733+mable-yip@users.noreply.github.com>
2021-09-28 15:32:28 +1000

    Implement sorting option in layout 1 and 2

--- scholly/base/src/layouts/layout1/components/publications/Publications.js
+++ scholly/base/src/layouts/layout1/components/publications/Publications.js
@@ -3,22 +3,30 @@
  */
 import React from 'react';
 import { Accordion } from 'react-bootstrap';
-import Publication from './publication/Publication';
-import { TEAM_PUBLICATIONS } from '../../../../global/data';
-import usePagination from '../shared/usePagination';
-import { pageSize } from '../../../../shared/config/publications';
+import GroupByCategory from './publicationLayout/GroupByCategory';
+import GroupByNone from './publicationLayout/GroupByNone';
+import { TEAM_PUBLICATIONS, TEAM_SITE_METADATA } from '../../../../global/data';
+import sortPublications from '../../../../shared/sortPublications';
+import { groupByOptions } from '../../../../shared/config/publications';
 
 const Publications = () => {
-  const { currentData, pagination } = usePagination(TEAM_PUBLICATIONS, pageSize);
+  const { publicationOptions } = TEAM_SITE_METADATA;
+  const publications = sortPublications(TEAM_PUBLICATIONS, publicationOptions.sortBy);
+
+  const renderPublications = () => {
+    switch (publicationOptions.groupBy) {
+      case groupByOptions.CATEGORY:
+        return <GroupByCategory teamPublications={publications} />;
+      default:
+        return <GroupByNone teamPublications={publications} />;
+    }
+  };
 
   return (
     <>
       <Accordion>
-        {currentData().map((pub) => (
-          <Publication pub={pub} key={pub._id} />
-        ))}
+        {renderPublications()}
       </Accordion>
-      {pagination()}
     </>
   );
 };
