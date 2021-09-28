8dcfdd7a8796a69e118982a8b98f064e6dd7bdec
mable-yip <47068733+mable-yip@users.noreply.github.com>
2021-09-28 11:29:01 +1000

    improve css styling

--- scholly/base/src/layouts/layout2/components/team/mobileView/TeamMemberMobile.js
+++ scholly/base/src/layouts/layout2/components/team/mobileView/TeamMemberMobile.js
@@ -19,7 +19,7 @@ const TeamMemberMobile = ({ teamMembers }) => {
     teamMembers.map((member, index) => (
       <Card
         style={{
-          marginBottom: '15px',
+          marginBottom: '10px',
           width: '100%',
           height: 'auto',
           boxShadow: '0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)',
@@ -31,18 +31,21 @@ const TeamMemberMobile = ({ teamMembers }) => {
         onBlur={handleMouseLeave}
       >
         <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
-          <div style={{ height: 'auto', minWidth: '150px' }}>
+          <div style={{
+            height: 'auto', minWidth: '150px', maxWidth: '150px',
+          }}
+          >
             <Card.Img style={{ width: '100%', height: '100%', objectFit: 'cover' }} src={profilePicture} />
           </div>
-          <Card.Body>
-            <Card.Title as="h5">{member.fullName}</Card.Title>
-            <Card.Text as="h6">
-              {member.position}
-            </Card.Text>
-            <Card.Text as="h7" style={{ color: hoveredMember === index ? 'var(--researchify-text-color)' : 'var(--researchify-text-color-secondary)' }}>
-              {member.summary}
-            </Card.Text>
-          </Card.Body>
+          <div>
+            <Card.Body>
+              <Card.Title style={{ fontSize: '17px' }}>{member.fullName}</Card.Title>
+              <Card.Text style={{ fontSize: '16px' }}>{member.position}</Card.Text>
+              <Card.Text style={{ fontSize: '14px', color: hoveredMember === index ? 'var(--researchify-text-color)' : 'var(--researchify-text-color-secondary)' }}>
+                {member.summary}
+              </Card.Text>
+            </Card.Body>
+          </div>
         </div>
       </Card>
     ))
