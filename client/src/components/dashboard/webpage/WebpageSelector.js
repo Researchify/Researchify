458a4e8e6d2c0fff81ac56a473d7ea403a003fec
vgoh1 <vgoh0001@student.monash.edu>
2021-10-06 15:41:51 +1100

    Add tooltip for disabled button

--- client/src/components/dashboard/webpage/WebpageSelector.js
+++ client/src/components/dashboard/webpage/WebpageSelector.js
@@ -1,26 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import {
-  Modal, Row,
+  Modal, Row, Tooltip, OverlayTrigger,
 } from 'react-bootstrap';
 import { PropTypes } from 'prop-types';
 import WebpageCard from './WebpageCard';
 import { addPage } from '../../../actions/website';
 import { availablePages as pages, pageDescriptions } from '../../../config/clientWebsite';
 import { PrimaryButton } from '../../shared/styledComponents';
+import ConditionalWrapper from '../../shared/ConditionalWrapper';
 
 const WebpageSelector = ({
   currentWebPages, teamId, closeModal, displayModal,
 }) => {
   const dispatch = useDispatch();
-  // All our web-page offerings
-  // webpageOfferings = availablePages - currentWebPages
-  // const webpageOfferings = availablePages.filter(
-  //   (page) => !currentWebPages.includes(page),
-  // );
-  const [availPages, setAvailPages] = useState([]);
-  console.log('available ', availPages);
-  console.log('cuurent', currentWebPages);
+
+  const [availablePages, setAvailablePages] = useState([]);
 
   // To control disabling the 'Next' Button in the pop-up
   const [displayButton, setDisplayButton] = useState(true);
@@ -48,43 +43,60 @@ const WebpageSelector = ({
   };
 
   useEffect(() => {
-    console.log(pages.filter(
-      (page) => !currentWebPages.includes(page),
-    ));
-    setAvailPages(pages.filter(
+    // calculate the available pages based on selected pages
+    setAvailablePages(pages.filter(
       (page) => !currentWebPages.includes(page),
     ));
   }, [currentWebPages]);
+
+  const renderDisableConfirmButtonTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Please select a page to add
+    </Tooltip>
+  );
 
   return (
     <>
       <Modal show={displayModal} onHide={closeModal} centered size="lg">
         <Modal.Header closeButton>
           <Modal.Title id="contained-modal-title-center">
-            Choose the Page you want to add to your website
+            Choose the page you want to add to your website
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <div style={{ margin: 'auto', width: '74%' }}>
             <Row xs={1} md={2} className="g-4">
-              {availPages.map((pageName) => (
-
+              {availablePages.map((pageName) => (
                 <WebpageCard
                   page={pageName}
                   description={pageDescriptions[pageName]}
                   handlePageSelection={handlePageSelection}
                 />
-
               ))}
             </Row>
           </div>
-          <PrimaryButton
-            className="float-right"
-            disabled={displayButton}
-            onClick={handleSubmit}
+          <ConditionalWrapper
+            condition={displayButton}
+            wrapper={(children) => (
+              <OverlayTrigger
+                placement="bottom"
+                overlay={renderDisableConfirmButtonTooltip}
+              >
+                {children}
+              </OverlayTrigger>
+            )}
           >
-            Confirm
-          </PrimaryButton>
+            <div style={{ display: 'inline-block', cursor: 'not-allowed', float: 'right' }}>
+              <PrimaryButton
+                disabled={displayButton}
+                onClick={handleSubmit}
+                style={displayButton ? { pointerEvents: 'none' } : {}}
+              >
+                Confirm
+              </PrimaryButton>
+            </div>
+
+          </ConditionalWrapper>
         </Modal.Body>
       </Modal>
     </>
