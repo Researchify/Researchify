37bc981d481c2122c700822ad14dd644972db808
vgoh1 <vgoh0001@student.monash.edu>
2021-10-06 14:24:53 +1100

    Add logic for selecting multiple webpages

--- client/src/components/dashboard/webpage/WebpageCard.js
+++ client/src/components/dashboard/webpage/WebpageCard.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Form } from 'react-bootstrap';
 import { PropTypes } from 'prop-types';
 
-const WebpageCard = ({ page, description }) => (
-  <>
-    <Card style={{ width: '18rem' }} onClick={console.log(page)}>
-      <Card.Body>
-        <Card.Title>{page}</Card.Title>
-        <Card.Text>
-          {description}
-        </Card.Text>
-      </Card.Body>
-    </Card>
-  </>
-);
+const WebpageCard = ({
+  page, description, handlePageSelection,
+}) => {
+  const handleChange = () => {
+    handlePageSelection(page);
+  };
+  return (
+    <>
+      <Form.Group>
+        <Card style={{ width: '18rem' }}>
+          <Card.Body>
+            <Form.Check type="checkbox" onChange={handleChange} />
+            <Card.Title>
+              {page}
+            </Card.Title>
+            <Card.Text>
+              {description}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Form.Group>
+    </>
+  );
+};
 
 WebpageCard.propTypes = {
   page: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  handlePageSelection: PropTypes.func.isRequired,
 };
 
 export default WebpageCard;
--- client/src/components/dashboard/webpage/WebpageSelector.js
+++ client/src/components/dashboard/webpage/WebpageSelector.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import {
-  Modal, DropdownButton, Dropdown, Row,
+  Modal, Row,
 } from 'react-bootstrap';
 import { PropTypes } from 'prop-types';
 import WebpageCard from './WebpageCard';
@@ -16,24 +16,42 @@ const WebpageSelector = ({
   // All our web-page offerings
   const availablePages = pages;
   // webpageOfferings = availablePages - currentWebPages
-  const webpageOfferings = availablePages.filter(
+  // const webpageOfferings = availablePages.filter(
+  //   (page) => !currentWebPages.includes(page),
+  // );
+  const [availPages, setAvailPages] = useState(availablePages.filter(
     (page) => !currentWebPages.includes(page),
-  );
-  const pagePlaceholder = 'Select page to add';
-
-  const [selectedPage, setSelectedPage] = useState(pagePlaceholder);
+  ));
+  console.log(availPages);
 
   // To control disabling the 'Next' Button in the pop-up
   const [displayButton, setDisplayButton] = useState(true);
 
-  const handlePageSelection = (e) => {
-    setSelectedPage(e);
-    setDisplayButton(false);
+  const [selectedPages, setSelectedPages] = useState([]);
+
+  const refreshWebpagesOfferings = () => {
+    setAvailPages(availPages.filter(
+      (page) => !currentWebPages.includes(page),
+    ));
+  };
+
+  const handlePageSelection = (page) => {
+    if (!selectedPages.includes(page)) {
+      selectedPages.push(page);
+    } else {
+      selectedPages.pop(page);
+    }
+    setSelectedPages(selectedPages);
+    if (selectedPages.length > 0) {
+      setDisplayButton(false);
+    } else {
+      setDisplayButton(true);
+    }
+    refreshWebpagesOfferings();
   };
 
   const handleSubmit = () => {
-    dispatch(addPage(teamId, selectedPage));
-    setSelectedPage(pagePlaceholder);
+    dispatch(addPage(teamId, selectedPages));
     closeModal();
   };
 
@@ -46,22 +64,14 @@ const WebpageSelector = ({
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <DropdownButton
-            className="mx-10"
-            id="dropdown-basic-button"
-            variant="secondary"
-            title={selectedPage}
-            onSelect={handlePageSelection}
-          >
-            {webpageOfferings.map((pageName) => (
-              <Dropdown.Item key={pageName} eventKey={pageName}>
-                {pageName}
-              </Dropdown.Item>
-            ))}
-          </DropdownButton>
-          {console.log(webpageOfferings)}
           <Row xs={1} md={2} className="g-4">
-            {webpageOfferings.map((pageName) => (<WebpageCard page={pageName} description={pageDescriptions[pageName]} />))}
+            {availPages.map((pageName) => (
+              <WebpageCard
+                page={pageName}
+                description={pageDescriptions[pageName]}
+                handlePageSelection={handlePageSelection}
+              />
+            ))}
           </Row>
           <PrimaryButton
             className="float-right"
