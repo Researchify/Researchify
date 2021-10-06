b37832fcb9f1aa89508f505028bfb7bb2fca7c36
rpra0006 <rpra0006@student.monash.edu>
2021-10-06 13:08:58 +0700

    Update team member pic for layout 1

--- scholly/base/src/layouts/layout1/components/team/TeamMember.js
+++ scholly/base/src/layouts/layout1/components/team/TeamMember.js
@@ -4,11 +4,10 @@
 
 import React from 'react';
 import { Card } from 'react-bootstrap';
-import profilePicture from '../../../../shared/images/profilepic.jpg';
 
 const TeamMember = ({ member }) => (
   <Card className="team-card">
-    <Card.Img variant="top" src={profilePicture} />
+    <Card.Img variant="top" src={member.memberPic} />
     <Card.Body>
       <div className="member-name">{member.fullName}</div>
       <div className="member-position">{member.position}</div>
