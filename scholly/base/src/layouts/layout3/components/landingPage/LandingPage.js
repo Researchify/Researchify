24afb44eda288a65f522261048f5ef716cef9059
Andrew Cao <ancao@Andrews-MacBook-Pro.local>
2021-10-09 01:16:56 +1100

    AAdded recent publications for landing page

--- scholly/base/src/layouts/layout3/components/landingPage/LandingPage.js
+++ scholly/base/src/layouts/layout3/components/landingPage/LandingPage.js
@@ -6,6 +6,7 @@ import { Container } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
 import { TEAM_HOMEPAGE, TEAM_INFO } from '../../../../global/data';
 import landingPicture from '../../../../shared/images/glenn-carstens-peters-npxXWgQ33ZQ-unsplash.jpg';
+import RecentPublications from '../publications/RecentPublications';
 
 const landingPage = () => {
   const homepageData = TEAM_HOMEPAGE;
@@ -22,13 +23,16 @@ const landingPage = () => {
         </title>
       </Helmet>
       <Container fluid className="pages-top-padding pages-side-padding">
-        <div className="landing-center-title">About Us</div>
         <Container fluid className="container-body">
           {/* eslint-disable-next-line jsx-a11y/alt-text */}
           <img src={landingPicture} align="left" className="left landing-image" style={{ width: '35%' }} />
           {homepageData.aboutUs.map((paragraph) => (
             <div className="landing-center-content">{paragraph}</div>
           ))}
+          <div className="recent-publications-title">Recent Publications</div>
+          <div className="recent-publications-body">
+            <RecentPublications />
+          </div>
         </Container>
       </Container>
     </>
---
+++ scholly/base/src/layouts/layout3/components/publications/RecentPublications.js
@@ -0,0 +1,24 @@
+/**
+ * The Publications component displays a list of publications.
+ */
+import React from 'react';
+import { Accordion } from 'react-bootstrap';
+import Publication from './publication/Publication';
+import { TEAM_PUBLICATIONS } from '../../../../global/data';
+
+const RecentPublications = () => {
+  let teamPublications = TEAM_PUBLICATIONS;
+  teamPublications.sort((a, b) => ((a.yearPublished < b.yearPublished) ? 1 : -1));
+  teamPublications = teamPublications.slice(0, 5);
+  return (
+    <>
+      <Accordion>
+        {teamPublications.map((pub) => (
+          <Publication pub={pub} key={pub._id} />
+        ))}
+      </Accordion>
+    </>
+  );
+};
+
+export default RecentPublications;
