7ac79dccb136eb67069b6e22da8d2d5e3a30c426
Sharan Sharabinth <ssha0054@student.monash.edu>
2021-10-10 17:07:12 +1100

    Trying to access and use cookies for tests

--- api/test/routes/achievements.test.js
+++ api/test/routes/achievements.test.js
@@ -4,6 +4,7 @@
 const supertest = require('supertest');
 
 const Achievement = require('../../src/models/achievement.model');
+const Team = require('../../src/models/team.model');
 const {
   connectDb,
   closeConnection,
@@ -12,14 +13,16 @@ const {
   seedDb,
 } = require('../util/db');
 const app = require('../../src/server');
-const { isTag } = require('domhandler');
 
 const request = supertest(app);
 
 /// The database used for this test suite.
 const DB_NAME = 'researchify_test_achievements';
 /// The homepage route prefix.
-const ROUTE_PREFIX = '/achievements'; 
+const ROUTE_PREFIX = '/achievements';
+
+/// Store an array of cookies retrieved from login session.
+let cookies;
 
 beforeAll(async () => {
   await connectDb(DB_NAME);
@@ -32,19 +35,60 @@ afterAll(async () => {
 
 beforeEach(async () => {
   await seedDb();
+
+  // Create a new team
+  let data = {
+    teamName: "testTeam",
+    orgName: "testOrg",
+    email: "testemail@gmail.com",
+    password: "testpass",
+  }
+
+  let res = await request.post('/team').send(data);
+
+  // Login with new team created and store cookie to global variable
+  data = {
+    email: "testemail@gmail.com",
+    password: "testpass",
+  }
+  res = await request.post('/auth/login').send(data);
+  console.log(res.headers);
+  console.log(res.headers['set-cookie']);
+  cookies = res.headers['set-cookie'];
+  //console.log(res);
 });
 
 afterEach(async () => {
   await clearAllCollections();
 });
 
-// POST request for invalid team id for an achievement
+// POST request tests
 describe('POST /achievements/:teamId', () => {
   it('should return 404 for a non-existent team', async() => {
-    const res = await request.post(`${ROUTE_PREFIX}/613b888ffca059539f01fc64`);
+    res = await request.post(`${ROUTE_PREFIX}/613b888ffca059539f01fc64`);
     expect(res.status).toBe(404);
   })
-})
+
+  it('should add achievement to team, async', async () => {
+    const team = await Team.findOne({email: "testemail@gmail.com"});
+    console.log(team);
+    data = {
+      title: "Best team",
+      yearAwarded: 2019,
+      description: "We are the best team.",
+      teamId: team._id.toString(),
+    };
+    
+    res = await request.post(`${ROUTE_PREFIX}/${team._id.toString()}`)
+      .set('Cookie', cookies)
+      .send(data);
+    expect(res.status).toBe(201);
+    expect(res.body.teamId).toEqual(team._id.toString());
+    expect(res.body.title).toEqual("Best team");
+    expect(res.body.yearAwarded).toEqual(2019);
+    expect(res.body.description).toEqual("We are the best team.");
+  });
+});
 
 // // test connection
 // it('should connect to our API', async () => {
