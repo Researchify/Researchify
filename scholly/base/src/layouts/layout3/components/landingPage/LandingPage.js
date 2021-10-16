597905b13f415dab26373abdb6ea00da5369a7ca
Mel <mkur0011@student.monash.edu>
2021-10-17 00:26:15 +0400

    Bug Fix: Shorter Text

--- scholly/base/src/layouts/layout3/components/landingPage/LandingPage.js
+++ scholly/base/src/layouts/layout3/components/landingPage/LandingPage.js
@@ -4,15 +4,10 @@
 import React from 'react';
 import { Row, Col, Container } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
-import { TEAM_HOMEPAGE, TEAM_INFO, TEAM_SITE_METADATA } from '../../../../global/data';
-import landingPicture from '../../../../shared/images/glenn-carstens-peters-npxXWgQ33ZQ-unsplash.jpg';
-import Favicon from 'react-favicon';
 import parse from 'html-react-parser';
-
-import { TEAM_HOMEPAGE, TEAM_INFO } from '../../../../global/data';
+import { TEAM_HOMEPAGE, TEAM_INFO, TEAM_SITE_METADATA } from '../../../../global/data';
 import RecentPublications from '../publications/RecentPublications';
 import TwitterFeed from '../twitter/TwitterFeed';
-import researchifyFavicon from '../../../../shared/images/favicon.ico';
 
 const landingPage = () => {
   const homepageData = TEAM_HOMEPAGE;
@@ -31,34 +26,35 @@ const landingPage = () => {
           {' '}
         </title>
       </Helmet>
-      <Favicon url={researchifyFavicon} />
       <Container fluid className="pages-top-padding pages-side-padding">
         <Container fluid className="container-body">
           {
-            profilePic && (<img src={profilePic} alt="Computer" align="left" className="left landing-image" style={{ width: '35%' }} />)
+              profilePic && (<img src={profilePic} alt="Computer" align="left" className="left landing-image" style={{ width: '35%' }} />)
           }
           <div className="landing-center-content">
             {parse(homepageData.aboutUs)}
           </div>
         </Container>
-        <Row className="container-body">
-          {
-            twitterHandle && (
-            <Col md={3} className="news-section">
-              <TwitterFeed linkedHandle={twitterHandle} themeOption={theme} twitterHeight={560} />
+        <br />
+        <Container fluid className="container-body">
+          <Row>
+            {
+              twitterHandle && (
+              <Col md={3} className="news-section">
+                <TwitterFeed linkedHandle={twitterHandle} themeOption={theme} twitterHeight={560} />
+              </Col>
+              )
+            }
+            <Col md={twitterHandle ? 9 : 12}>
+              <Container fluid className="container-recent-pub-body">
+                <div className="recent-publications-title">Recent Publications</div>
+                <div className="recent-publications-body">
+                  <RecentPublications />
+                </div>
+              </Container>
             </Col>
-            )
-          }
-          <Col md={twitterHandle ? 9 : 12}>
-            <Container fluid className="container-recent-pub-body">
-              <div className="recent-publications-title">Recent Publications</div>
-              <div className="recent-publications-body">
-                <RecentPublications />
-              </div>
-            </Container>
-          </Col>
-        </Row>
-
+          </Row>
+        </Container>
       </Container>
     </>
   );
