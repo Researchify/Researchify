1dde9a7777b99f360dd88d3395188ad7f644f0cc
melvinalex7 <53939479+melvinalex7@users.noreply.github.com>
2021-10-17 00:29:13 +0400

    Twitter Component For Layout 3 (#281)
    * Add Twitter Component For Layout 3 Light and Dark Themes
    
    * Destructuring Template option
    
    * Destructuring Theme
    
    * Bug Fix: Shorter Text
    
    Co-authored-by: Mel <mkur0011@student.monash.edu>

--- scholly/base/src/layouts/layout3/components/landingPage/LandingPage.js
+++ scholly/base/src/layouts/layout3/components/landingPage/LandingPage.js
@@ -2,18 +2,19 @@
  * This file output landing page (homepage) of client-site.
  */
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
-import Favicon from 'react-favicon';
 import parse from 'html-react-parser';
-
-import { TEAM_HOMEPAGE, TEAM_INFO } from '../../../../global/data';
+import { TEAM_HOMEPAGE, TEAM_INFO, TEAM_SITE_METADATA } from '../../../../global/data';
 import RecentPublications from '../publications/RecentPublications';
-import researchifyFavicon from '../../../../shared/images/favicon.ico';
+import TwitterFeed from '../twitter/TwitterFeed';
 
 const landingPage = () => {
   const homepageData = TEAM_HOMEPAGE;
-  const { teamName, profilePic } = TEAM_INFO;
+  const { teamName, twitterHandle, profilePic } = TEAM_INFO;
+  const { template } = TEAM_SITE_METADATA;
+  const { theme } = template;
+
   return (
     <>
       <Helmet>
@@ -25,21 +26,34 @@ const landingPage = () => {
           {' '}
         </title>
       </Helmet>
-      <Favicon url={researchifyFavicon} />
       <Container fluid className="pages-top-padding pages-side-padding">
         <Container fluid className="container-body">
           {
-            profilePic && (<img src={profilePic} alt="Computer" align="left" className="left landing-image" style={{ width: '35%' }} />)
+              profilePic && (<img src={profilePic} alt="Computer" align="left" className="left landing-image" style={{ width: '35%' }} />)
           }
           <div className="landing-center-content">
             {parse(homepageData.aboutUs)}
           </div>
         </Container>
-        <Container fluid className="container-recent-pub-body">
-          <div className="recent-publications-title">Recent Publications</div>
-          <div className="recent-publications-body">
-            <RecentPublications />
-          </div>
+        <br />
+        <Container fluid className="container-body">
+          <Row>
+            {
+              twitterHandle && (
+              <Col md={3} className="news-section">
+                <TwitterFeed linkedHandle={twitterHandle} themeOption={theme} twitterHeight={560} />
+              </Col>
+              )
+            }
+            <Col md={twitterHandle ? 9 : 12}>
+              <Container fluid className="container-recent-pub-body">
+                <div className="recent-publications-title">Recent Publications</div>
+                <div className="recent-publications-body">
+                  <RecentPublications />
+                </div>
+              </Container>
+            </Col>
+          </Row>
         </Container>
       </Container>
     </>
