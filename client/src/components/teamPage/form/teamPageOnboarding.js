6c76f150b08bca957275b63479ca2bad44ecbd07
jagan <jvij0003@student.monash.edu>
2021-10-19 00:08:33 +0300

    initial team page onboarding file

---
+++ client/src/components/teamPage/form/teamPageOnboarding.js
@@ -0,0 +1,10 @@
+/**
+ * This page contains the code for team page onboarding
+ */
+
+/*
+import React, { useState } from 'react';
+import { Steps } from 'intro.js-react';
+import { BiHelpCircle } from 'react-icons/bi';
+import 'intro.js/introjs.css';
+*/
